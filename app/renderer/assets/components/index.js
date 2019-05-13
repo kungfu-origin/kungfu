10f92fc94153404d7c2f9d7093b81b5335f0da2b
yizhi <yizhi.zhang@taurus.ai>
2019-05-13 17:47:56 +0800

    [add] export data

--- app/renderer/assets/components/index.js
+++ app/renderer/assets/components/index.js
@@ -57,42 +57,6 @@ const saveFile = ({
     })
 }
 
-//message 换
-const Message = {
-    error: function(message){
-        Vue.notify({
-            title: '错误',
-            message: message,
-            position: "bottom-right",
-            type: 'error'
-        })
-    },
-    warning: function(message){
-        Vue.notify({
-            title: '警告',
-            message: message,
-            position: "bottom-right",
-            type: 'warning'
-        })
-    },
-    success: function(message){
-        Vue.notify({
-            title: '成功',
-            message: message,
-            position: "bottom-right",
-            type: 'success'
-        })
-    },
-
-    start: function(message){
-        Vue.notify({
-            title: '启动中',
-            message: message,
-            position: "bottom-right",
-            type: 'info'
-        })
-    }
-}
 
 
 
@@ -103,9 +67,6 @@ export default function (Vue) {
     })
 
     Vue.saveFile = Vue.prototype.$saveFile = saveFile;
-<<<<<<< HEAD
-
-
 
     //message 换
     const Message = {
@@ -143,8 +104,6 @@ export default function (Vue) {
             })
         }
     }
-=======
->>>>>>> [add] export csv
     Vue.message = Vue.prototype.$message = Message
 }
 
