d86fc20c17bdec34e8a3d6ef0c14e244c8fe1b47
Keren Dong <dongkeren@gmail.com>
2019-05-26 11:41:59 +0800

    Use KF_NO_EXT to disable extensions

--- python/extensions/__init__.py
+++ python/extensions/__init__.py
@@ -1,11 +1,18 @@
-import sys, traceback
+import os, sys, traceback
+import pkgutil
 from kungfu.log.Logger import create_logger
-kfext_logger = create_logger('extensions', 'error')
+
+kfext_log_level = os.getenv('KF_LOG_LEVEL')
+if not kfext_log_level:
+    kfext_log_level = 'error'
+kfext_logger = create_logger('extensions', kfext_log_level)
 
 class ExtensionRegistry:
-    def __init__(self):
+    def __init__(self, ext_type):
         self._registry = {}
+        self._ext_type = ext_type
     def register_extension(self, name, extension):
+        kfext_logger.debug('Registering %s extension %s', self._ext_type, name)
         self._registry[name] = extension
     def has_extension(self, name):
         return name in self._registry
@@ -14,15 +21,15 @@ class ExtensionRegistry:
     def names(self):
         return self._registry.keys()
 
-EXTENSION_REGISTRY_MD=ExtensionRegistry()
-EXTENSION_REGISTRY_TD=ExtensionRegistry()
+EXTENSION_REGISTRY_MD=ExtensionRegistry('MD')
+EXTENSION_REGISTRY_TD=ExtensionRegistry('TD')
 
-import pkgutil
-__path__ = pkgutil.extend_path(__path__, __name__)
-for importer, modname, ispkg in pkgutil.walk_packages(path=__path__, prefix=__name__+'.'):
-    try:
-        __import__(modname)
-        kfext_logger.info('Loaded extension %s', modname)
-    except:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        kfext_logger.critical('Bad extension %s, %s %s', modname, exc_type, traceback.format_exception(exc_type, exc_obj, exc_tb))
+if not os.getenv('KF_NO_EXT'):
+    __path__ = pkgutil.extend_path(__path__, __name__)
+    for importer, modname, ispkg in pkgutil.iter_modules(path=__path__, prefix=__name__+'.'):
+        try:
+            __import__(modname)
+            kfext_logger.info('Loaded extension %s', modname[len(__name__)+1:])
+        except:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            kfext_logger.critical('Bad extension %s, %s %s', modname, exc_type, traceback.format_exception(exc_type, exc_obj, exc_tb))
--- python/kungfu/__main__.py
+++ python/kungfu/__main__.py
@@ -1,3 +1,9 @@
+# Environment variables that affects:
+#   KF_HOME - base folder for kungfu files
+#   KF_LOG_LEVEL - logging level
+#   KF_NO_EXT - disable extensions if set
+
+import os
 import kungfu.command as kfc
 import kungfu.service
 import kungfu.client
