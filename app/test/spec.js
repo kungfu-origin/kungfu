80212adda0f2bbbc9e772354db2a484ff1d82ee2
yizhi.zhang <zhangyiizhi@126.com>
2019-07-16 20:18:34 +0800

    [add] init test

---
+++ app/test/spec.js
@@ -0,0 +1,30 @@
+const Application = require('spectron').Application
+const assert = require('assert');
+const path = require('path');
+const $ = require('jquery');
+
+describe('Application launch', function () {
+    this.timeout(10000)
+
+    before(function(){
+        this.app = new Application({
+        // Your electron path can be any binary
+        // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
+        // But for the sake of the example we fetch it from our node_modules.
+            path: '/Applications/Kungfu.Trader.app/Contents/MacOS/Kungfu.Trader',
+            args: [path.join(__dirname, '..')]
+        })
+        return this.app.start()
+    })
+
+    after(function () {
+        if (this.app && this.app.isRunning()) {
+          return this.app.stop()
+        }
+      })
+
+    it('shows an initial window', function (done) {
+       console.log($('body').innerHTML)
+       done()
+    })
+})
