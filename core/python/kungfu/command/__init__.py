69e7666d62f8774f0f9cde133c2ee9d3fa81073b
Keren Dong <dongkeren@gmail.com>
2019-07-25 18:19:34 +0800

    Set KF_HOME default + Remove tora related files

--- core/python/kungfu/command/__init__.py
+++ core/python/kungfu/command/__init__.py
@@ -1,16 +1,29 @@
 import os
+import platform
 import click
 import kungfu.yijinjing.journal as kfj
 import pyyjj
 
 
 @click.group(invoke_without_command=True)
-@click.option('-H', '--home', type=str, required=True, help='kungfu home folder')
+@click.option('-H', '--home', type=str, help='kungfu home folder, defaults to APPDATA/kungfu/app, where APPDATA defaults to %APPDATA% on windows, $XDG_CONFIG_HOME on linux, ~/Library/Application Support on mac')
 @click.option('-l', '--log_level', type=click.Choice(['trace', 'debug', 'info', 'warning', 'error', 'critical']),
               default='warning', help='logging level')
 @click.option('-n', '--name', type=str, help='name for the process, defaults to command if not set')
 @click.pass_context
 def kfc(ctx, home, log_level, name):
+    if not home:
+        osname = platform.system()
+        user_home = os.path.expanduser('~')
+        if osname == 'Linux':
+            xdg_config_home = os.getenv('XDG_CONFIG_HOME')
+            home = xdg_config_home if xdg_config_home else os.path.join(user_home, '.config')
+        if osname == 'Darwin':
+            home = os.path.join(user_home, 'Library', 'Application Support')
+        if osname == 'Windows':
+            home = os.getenv('APPDATA')
+        home = os.path.join(home, 'kungfu', 'app')
+
     os.environ['KF_HOME'] = ctx.home = home
     os.environ['KF_LOG_LEVEL'] = ctx.log_level = log_level
 
