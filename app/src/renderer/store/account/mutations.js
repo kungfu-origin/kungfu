fd0c4f9fb116876c3b33a74ed6b5866af08e3486
yizhi.zhang <zhangyiizhi@126.com>
2019-08-27 18:44:48 +0800

    [fix] vuex update data

--- app/src/renderer/store/account/mutations.js
+++ app/src/renderer/store/account/mutations.js
@@ -1,9 +1,11 @@
+import Vue from 'vue';
 export const SET_ONE_MD_TD_STATE = (state, { id, stateData }) => {
-    state.mdTdState[id] = stateData
+    Vue.set(state.mdTdState, id, stateData)
 }
 
 export const DELETE_ONE_MD_TD_STATE = (state, id) => {
     delete state.mdTdState[id]
+    Vue.delete(state.mdTdState, id)    
 }
 
 export const SET_ACCOUNTS_ASSET = (state, accountsAsset) => {
@@ -11,7 +13,7 @@ export const SET_ACCOUNTS_ASSET = (state, accountsAsset) => {
 }
 
 export const SET_ACCOUNT_ASSET = (state, {accountId, accountAsset}) => {
-    state.accountsAsset[accountId] = accountAsset
+    Vue.set(state.accountAsset, accountId, accountAsset)
 }
 
 export const SET_ACCOUNT_LIST = (state, accountList) => {
