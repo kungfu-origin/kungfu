6cdc501a0c7398a49f9fe8a9150e8e1f812b0b82
Keren Dong <dongkeren@gmail.com>
2019-09-06 18:00:06 +0800

    Use c++1x to convert encoding

--- core/cpp/wingchun/include/kungfu/wingchun/encoding.h
+++ core/cpp/wingchun/include/kungfu/wingchun/encoding.h
@@ -5,60 +5,39 @@
 #ifndef KUNGFU_ENCODING_H
 #define KUNGFU_ENCODING_H
 
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <cstdlib>
-#if defined(_WIN32) || defined(_WIN64)
-#include <windows.h>
+#include<locale>
+#include<codecvt>
+
+#ifdef _WINDOWS
+#define GB_ENCODING_NAME "zh-CN"
 #else
-#include <iconv.h>
+#define GB_ENCODING_NAME "zh_CN.GB18030"
 #endif
 
 namespace kungfu
 {
     namespace wingchun
     {
-#ifdef _WIN32
-        inline std::string gbk2utf8(const std::string& src_str)
+        // utility wrapper to adapt locale-bound facets for wstring/wbuffer convert
+        template<class Facet>
+        struct deletable_facet : Facet
         {
-            int len = MultiByteToWideChar(CP_ACP, 0, src_str.c_str(), -1, NULL, 0);
-            wchar_t* wstr = new wchar_t[len + 1];
-            memset(wstr, 0, len + 1);
-            MultiByteToWideChar(CP_ACP, 0, src_str.c_str(), -1, wstr, len);
-            len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
-            char* str = new char[len + 1];
-            memset(str, 0, len + 1);
-            WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
-            std::string strTemp = str;
-            if (wstr) delete[] wstr;
-            if (str) delete[] str;
-            return strTemp;
-        }
-#else
-        inline std::string gbk2utf8(const std::string& src_str)
+            template<class ...Args>
+            deletable_facet(Args &&...args) : Facet(std::forward<Args>(args)...)
+            {}
+
+            ~deletable_facet()
+            {}
+        };
+
+        typedef deletable_facet<std::codecvt_byname<wchar_t, char, std::mbstate_t>> codec_facet_t;
+
+        std::string gbk2utf8(const std::string &str)
         {
-            iconv_t conv = iconv_open("utf8", "gbk");
-            if (conv == (iconv_t)-1)
-                return "";
-
-            char* src = (char*)src_str.c_str();
-            char dst[4096] = {0};
-            char* out = dst;
-            size_t inbytes = strlen(src);
-            size_t outbytes = 4096;
-
-            if (iconv(conv, &src, &inbytes, &out, &outbytes) == (size_t)-1)
-            {
-                return "";
-            }
-            iconv_close(conv);
-
-            dst[4096 - outbytes] = 0;
-
-            return std::string(dst);
+            std::wstring_convert<codec_facet_t> cv_gb(new codec_facet_t(GB_ENCODING_NAME));
+            std::wstring_convert<std::codecvt_utf8<wchar_t>> cv_utf8;
+            return cv_utf8.to_bytes(cv_gb.from_bytes(str));
         }
-#endif
     }
 }
 
