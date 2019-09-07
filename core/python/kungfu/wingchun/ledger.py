603c7b8373e6069594a18c66f0f7b54ac82c61c8
qlu <qing.lu@taurus.ai>
2019-09-07 14:48:59 +0800

    fix future position direction

--- core/python/kungfu/wingchun/ledger.py
+++ core/python/kungfu/wingchun/ledger.py
@@ -143,6 +143,7 @@ class Ledger(pywingchun.Ledger):
     def on_future_account(self, asset, position_details):
         pos_objects = []
         for uid, details in groupby(position_details, key=lambda e: get_position_uid(e.instrument_id, e.exchange_id, e.direction)):
+            direction = details[0].direction
             detail_objects = []
             instrument_info = None
             for detail in sorted(details, key=lambda detail: (detail.open_date, detail.trade_time)):
@@ -153,7 +154,7 @@ class Ledger(pywingchun.Ledger):
                              "long_margin_ratio": instrument_info["long_margin_ratio"],
                              "short_margin_ratio": instrument_info["short_margin_ratio"]})
                 detail_objects.append(FuturePositionDetail(**args))
-            pos_args = {"details": detail_objects, "trading_day": self.ctx.trading_day}
+            pos_args = {"details": detail_objects, "trading_day": self.ctx.trading_day, "direction": direction}
             pos_args.update(instrument_info)
             pos = FuturePosition(**pos_args)
             pos_objects.append(pos)
