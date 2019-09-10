847db22245332b23325821ce5133156bd2432b89
qlu <qing.lu@taurus.ai>
2019-09-10 17:04:29 +0800

    fix edit account

--- core/python/kungfu/command/account/edit.py
+++ core/python/kungfu/command/account/edit.py
@@ -13,6 +13,8 @@ def edit(ctx, receive_md, id):
     account_data = ctx.db.find_account(account_id)
     if account_data:
         answers = encrypt(ctx.schema, prompt(make_questions(ctx.schema, account_data['config'])))
+        if receive_md:
+            ctx.db.reset_receive_md()
         receive_md = receive_md or account_data['receive_md']
         ctx.db.add_account(account_id=account_id, source_name=ctx.source, receive_md=receive_md, config=answers)
     else:
