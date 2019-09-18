2d72de96e6edc7e7fd55045c39c1b20b3212398e
swang <shuai.wang@taurus.ai>
2019-09-18 16:41:02 +0800

    [fix] little issue

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -75,7 +75,9 @@ program
             .finally(() => process.exit(0));
     })
 
-if (process.env.NODE_ENV === 'development') {
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV !== 'production') {
+    console.log(process.argv,'---')
     program.parse(process.argv)
 } else {
     //@ts-ignore
