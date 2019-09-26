42d8cfd4e4593b867b0164b950613daf3655e849
yizhi.zhang <zhangyiizhi@126.com>
2019-09-26 19:52:09 +0800

    [fix] commit version

--- app/.electron-kungfu/webpack.main.config.js
+++ app/.electron-kungfu/webpack.main.config.js
@@ -10,13 +10,16 @@ const OptimizeJsPlugin = require("optimize-js-plugin");
 var fs = require("fs");
 var gitCommitVersion = '';
 try {
-  console.log(__dirname, '..', '..', '.git', 'HEAD')
   var gitHEAD = fs.readFileSync(path.join(__dirname, '..', '..', '.git', 'HEAD'), 'utf-8').trim() // ref: refs/heads/develop
-  var ref = gitHEAD.split(': ')[1] // refs/heads/develop
-  var develop = gitHEAD.split('/')[2] // 环境：develop
-  console.log(ref, develop)
-  var gitVersion = fs.readFileSync(path.join(__dirname, '..', '..', '.git', ref), 'utf-8').trim() // git版本号，例如：6ceb0ab5059d01fd444cf4e78467cc2dd1184a66
-  gitCommitVersion = '"' + develop + ': ' + gitVersion + '"'  
+  if(gitHEAD.split[': '].length <= 1) {
+    gitCommitVersion = gitHEAD
+  } else {
+    var ref = gitHEAD.split(': ')[1] // refs/heads/develop
+    var develop = gitHEAD.split('/')[2] // 环境：develop
+    console.log(ref, develop)
+    var gitVersion = fs.readFileSync(path.join(__dirname, '..', '..', '.git', ref), 'utf-8').trim() // git版本号，例如：6ceb0ab5059d01fd444cf4e78467cc2dd1184a66
+    gitCommitVersion = '"' + develop + ': ' + gitVersion + '"'  
+  }
 } catch (err) {
   console.error(err)
 }
