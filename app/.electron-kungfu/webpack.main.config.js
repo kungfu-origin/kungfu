9196dcc69a0cee734ca2f7c92d33329331010fc2
yizhi.zhang <zhangyiizhi@126.com>
2019-09-26 19:22:04 +0800

    [fix]build

--- app/.electron-kungfu/webpack.main.config.js
+++ app/.electron-kungfu/webpack.main.config.js
@@ -7,13 +7,19 @@ const { dependencies } = require('../package.json')
 const webpack = require('webpack')
 const OptimizeJsPlugin = require("optimize-js-plugin");
 
-var fs = require("fs")
-console.log(__dirname, '..', '..', '.git', 'HEAD')
-var gitHEAD = fs.readFileSync(path.join(__dirname, '..', '..', '.git', 'HEAD'), 'utf-8').trim() // ref: refs/heads/develop
-var ref = gitHEAD.split(': ')[1] // refs/heads/develop
-var develop = gitHEAD.split('/')[2] // 环境：develop
-var gitVersion = fs.readFileSync(path.join(__dirname, '..', '..', '.git', ref), 'utf-8').trim() // git版本号，例如：6ceb0ab5059d01fd444cf4e78467cc2dd1184a66
-var gitCommitVersion = '"' + develop + ': ' + gitVersion + '"'
+var fs = require("fs");
+var gitCommitVersion = '';
+try {
+  console.log(__dirname, '..', '..', '.git', 'HEAD')
+  var gitHEAD = fs.readFileSync(path.join(__dirname, '..', '..', '.git', 'HEAD'), 'utf-8').trim() // ref: refs/heads/develop
+  var ref = gitHEAD.split(': ')[1] // refs/heads/develop
+  var develop = gitHEAD.split('/')[2] // 环境：develop
+  console.log(ref, develop)
+  var gitVersion = fs.readFileSync(path.join(__dirname, '..', '..', '.git', ref), 'utf-8').trim() // git版本号，例如：6ceb0ab5059d01fd444cf4e78467cc2dd1184a66
+  gitCommitVersion = '"' + develop + ': ' + gitVersion + '"'  
+} catch (err) {
+  console.error(err)
+}
 
 let whiteListedModules = [
   'vue', 
