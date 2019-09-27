b2bc12b1a1f5c32d4d452d5ba7ca609ff2182dda
yizhi.zhang <zhangyiizhi@126.com>
2019-09-27 16:32:47 +0800

    [add] cli build

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-
 yarn clean
 cd ./core
 npm config set kungfu-core:runtime node
 yarn run build:cpp
 cd ../cli
 yarn run build
-cd ../core
-yarn clean
-npm config set kungfu-core:runtime electron
-yarn run build
-cd ../app
-yarn run build
+# cd ../core
+# pwd
+# yarn clean
+# npm config set kungfu-core:runtime electron
+# yarn run build
+# cd ../app
+# pwd
+# yarn run build
