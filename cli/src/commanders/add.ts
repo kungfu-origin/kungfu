03628e64ee022fdc5dcb3e5323991d10d5c1adbc
yizhi.zhang <zhangyiizhi@126.com>
2019-10-13 11:01:28 +0800

    [fix] add path

--- cli/src/commanders/add.ts
+++ cli/src/commanders/add.ts
@@ -126,7 +126,6 @@ function buildStrategyQuestion(strategyData: any, updateModule: boolean | undefi
             name: 'strategy_path',
             default: strategyData.strategy_path || os.homedir(),
             message: `Update strategy_path (absolute path)`,
-            directoryOnly: true,
             validate: (value: string) => {
                 let hasError = null;
                 !updateModule && requiredValidator(null, value, (err: Error) => hasError = err)
