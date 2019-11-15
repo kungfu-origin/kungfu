74e3eca63b4efb9bd843d1ce06f8396d3b74148f
yizhi.zhang <zhangyiizhi@126.com>
2019-11-15 16:11:44 +0800

    [add] remove dev tool

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -51,7 +51,7 @@ function createWindow () {
 	}
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools()
+	// mainWindow.webContents.openDevTools()
 
 	// // Emitted when the window is closed.
 	mainWindow.on('close', (e) => {
