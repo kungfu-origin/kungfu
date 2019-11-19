8d375c69ab3178fe65d82f2600b6a9e7c783636e
yizhi.zhang <zhangyiizhi@126.com>
2019-11-19 15:39:32 +0800

    [add] build process

--- build.sh
+++ build.sh
@@ -2,14 +2,16 @@
 
 yarn clean
 npm config set kungfu-core:runtime node
+yarn install
 
 cd ./core
 yarn run build
 cd ../cli
 yarn run build
 
-# npm config set kungfu-core:runtime electron
 
+# npm config set kungfu-core:runtime electron
+# yarn install
 # cd ../core
 # yarn run build
 # cd ../app
