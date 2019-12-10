9803791c8985808d9e848b93d6e4e58d8ec8f72e
ayizhi <zhangyiizhi@126.com>
2019-12-10 09:57:57 +0800

    [add] remove electron cli process

--- build-cli.sh
+++ build-cli.sh
@@ -8,10 +8,10 @@ yarn run build
 cd ../cli
 yarn run build-cli
 
-cd ../
-npm config set kungfu-core:runtime electron
-cd ./core
-yarn run clean
-yarn run build
+# cd ../
+# npm config set kungfu-core:runtime electron
+# cd ./core
+# yarn run clean
+# yarn run build
 cd ../app
 yarn run build
