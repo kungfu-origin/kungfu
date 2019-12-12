619cd65a1427003dcb8b7fa664127e002b05d10e
qlu <qing.lu@taurus.ai>
2019-12-12 15:43:44 +0800

    potential crash fix when seek to time

--- core/cpp/yijinjing/src/journal/journal.cpp
+++ core/cpp/yijinjing/src/journal/journal.cpp
@@ -58,10 +58,6 @@ namespace kungfu
                              time::strftime(current_page_->begin_time(), "%F %T"), time::strftime(current_page_->end_time(), "%F %T"));
                 while (current_page_->is_full() && current_page_->end_time() <= nanotime)
                 {
-                    while (frame_->has_data())
-                    {
-                        frame_->move_to_next();
-                    }
                     load_next_page();
                 }
                 while (frame_->has_data() && frame_->gen_time() <= nanotime)
