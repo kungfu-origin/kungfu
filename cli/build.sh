cb12174e16e863307b4d7d2180133ae3f4f58cb6
ayizhi <zhangyiizhi@126.com>
2019-12-24 16:10:31 +0800

    [add] resources

--- cli/build.sh
+++ cli/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 if [ "$(uname)" == "Darwin" ]; then
     npm run build:mac
+    cp -RF ./resources/mac/* ./build/cli 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     npm run build:linux
+    cp -RF ./resources/linux/* ./build/cli 
 fi
