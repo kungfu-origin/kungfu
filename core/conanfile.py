05a73ba19039204a17f2db509625acf2b15d418b
Keren Dong <dongkeren@gmail.com>
2020-02-21 16:44:28 +0800

    Add build_info.json

--- core/conanfile.py
+++ core/conanfile.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import json
+import getpass
+import platform
 import subprocess
 import datetime
 from conans import ConanFile
@@ -22,32 +25,64 @@ class KungfuCoreConan(ConanFile):
     options = {
         "log_level": ["trace", "debug", "info", "warning", "error", "critical"],
         "arch": ["x64"],
-        "runtime": ["electron", "node"],
+        "js_runtime": ["electron", "node"],
         "node_version": ["10.16.0"],
         "electron_version": ["4.2.11"]
     }
     default_options = {
         "log_level": "info",
         "arch": "x64",
-        "runtime": "electron",
+        "js_runtime": "electron",
         "node_version": "10.16.0",
         "electron_version": "4.2.11"
     }
 
     def build(self):
+        build_type = str(self.settings.build_type)
+        os.environ['CMAKE_BUILD_TYPE'] = build_type
+
         cmd = self._build_cmake_js_cmd('build')
-        sys.exit(subprocess.Popen(cmd).wait())
+        rc = subprocess.Popen(cmd).wait()
+
+        if rc != 0:
+            self.output.error('build failed')
+            sys.exit(rc)
+
+        self.output.success('source files compiled')
+
+        now = datetime.datetime.now()
+        build_date = now.strftime('%Y%m%d%H%M%S')
+
+        git = tools.Git()
+        git_revision = git.get_revision()[:8]
+
+        build_version = f'{self.version}.{build_date}.{git_revision}' if tools.Version(self.version).prerelease else self.version
+        with open(os.path.join(os.getcwd(), build_type, 'version.info'), 'w') as version_file:
+            version_file.write(build_version)
+
+        build_info = {
+            'version': build_version,
+            'pythonVersion': platform.python_version(),
+            'git': {
+                'tag': git.get_tag(),
+                'branch': git.get_branch(),
+                'revision': git.get_revision(),
+                'pristine': git.is_pristine()
+            },
+            'build': {
+                'user': getpass.getuser(),
+                'osVersion': tools.os_info.os_version,
+                'timestamp': now.strftime('%Y/%m/%d %H:%M:%S')
+            }
+        }
+        with open(os.path.join(os.getcwd(), build_type, 'build_info.json'), 'w') as output:
+            json.dump(build_info, output, indent=2)
+
+        self.output.success(f'build version {build_version}')
 
     def package(self):
         build_type = str(self.settings.build_type)
         os.environ['CMAKE_BUILD_TYPE'] = build_type
-
-        git = tools.Git()
-        git_revision = git.get_revision()[:8]
-        build_date = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-        build_version = f'{self.version}.{build_date}.{git_revision}' if tools.Version(self.version).prerelease else self.version
-        with open(os.path.join(os.getcwd(), build_type, 'version.info'), 'w') as version_file:
-            version_file.write(build_version)
 
         with tools.chdir('..'):
             rc = subprocess.Popen(['pipenv', 'run', 'pyinstaller', '--clean', '-y', '--distpath=build', 'python/kfc-conf.spec']).wait()
@@ -73,6 +108,9 @@ class KungfuCoreConan(ConanFile):
 
         self.output.success('setuptools done')
 
+    def _get_node_version(self):
+        return str(self.options.electron_version) if self.options.js_runtime == 'electron' else str(self.options.node_version)
+
     def _build_cmake_js_cmd(self, cmd):
         spdlog_levels = {
             'trace': 'SPDLOG_LEVEL_TRACE',
@@ -84,12 +122,11 @@ class KungfuCoreConan(ConanFile):
         }
         loglevel = spdlog_levels[str(self.options.log_level)]
         python_path = subprocess.Popen(['pipenv', '--py'], stdout=subprocess.PIPE).stdout.read().decode().strip()
-        runtime_version = str(self.options.electron_version) if self.options.runtime == 'electron' else str(self.options.node_version)
 
         cmake_js_cmd = [tools.which('yarn'), 'cmake-js', '--debug' if self.settings.build_type == 'Debug' else '',
                         '--arch', str(self.options.arch),
-                        '--runtime', str(self.options.runtime),
-                        '--runtime-version', runtime_version,
+                        '--runtime', str(self.options.js_runtime),
+                        '--runtime-version', self._get_node_version(),
                         '--CDPYTHON_EXECUTABLE=' + python_path,
                         '--CDSPDLOG_LOG_LEVEL_COMPILE=' + loglevel]
 
