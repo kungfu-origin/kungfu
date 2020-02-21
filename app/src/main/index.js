b25fdb7280268794cbd6b24c25680ea57e0be459
ayizhi <zhangyiizhi@126.com>
2020-02-21 13:53:58 +0800

    [add] trade orders positions

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -124,7 +124,7 @@ app.on('activate', function () {
 // On macOS it's common to re-create a window in the app when the
 // dock icon is clicked and there are no other windows open.
     if (mainWindow && mainWindow.isDestroyed()) createWindow()
-    else if(mainWindow.isVisible()) mainWindow.focus()
+    else if(mainWindow && mainWindow.isVisible()) mainWindow.focus()
     else mainWindow.show()
 })
 
