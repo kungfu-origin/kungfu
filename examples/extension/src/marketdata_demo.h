d16f4178fa2722bdf5f723171008a967417248fe
Keren Dong <dongkeren@gmail.com>
2020-03-18 15:34:50 +0800

    Update extension demo

--- examples/extension/src/marketdata_demo.h
+++ examples/extension/src/marketdata_demo.h
@@ -1,37 +1,27 @@
 #ifndef MD_KFEXT_DEMO_H
 #define MD_KFEXT_DEMO_H
 
-#include <string>
-#include <map>
-
-#include <kungfu/longfist/longfist.h>
 #include <kungfu/yijinjing/common.h>
 #include <kungfu/wingchun/broker/marketdata.h>
 
-namespace kungfu
+namespace kungfu::wingchun::kfext_demo
 {
-    namespace wingchun
+    class MarketDataDemo : public broker::MarketData
     {
-        namespace kfext_demo
-        {
-            class MarketDataDemo : public broker::MarketData
-            {
-            public:
-                MarketDataDemo(bool low_latency, yijinjing::data::locator_ptr locator,
-                          std::map<std::string, std::string> &config_str,
-                          std::map<std::string, int> &config_int,
-                          std::map<std::string, double> &config_double);
+    public:
+        MarketDataDemo(bool low_latency, yijinjing::data::locator_ptr locator,
+                       std::map<std::string, std::string> &config_str,
+                       std::map<std::string, int> &config_int,
+                       std::map<std::string, double> &config_double);
 
-                void on_start() override;
+        void on_start() override;
 
-                bool subscribe(const std::vector<longfist::types::Instrument> &instruments) override;
+        bool subscribe(const std::vector<longfist::types::Instrument> &instruments) override;
 
-                bool subscribe_all() override;
+        bool subscribe_all() override;
 
-                bool unsubscribe(const std::vector<longfist::types::Instrument> &instruments) override;
-            };
-        }
-    }
+        bool unsubscribe(const std::vector<longfist::types::Instrument> &instruments) override;
+    };
 }
 
 #endif //MD_KFEXT_DEMO_H
