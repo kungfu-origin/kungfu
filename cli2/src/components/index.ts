3340c965be5ff09185488d2607f4bbbad956145d
ayizhi <zhangyiizhi@126.com>
2020-03-20 11:52:18 +0800

    [add] tmp tradingdata cli monit

--- cli2/src/components/index.ts
+++ cli2/src/components/index.ts
@@ -1,4 +1,3 @@
-// import tradingData from '@/components/tradingData/index'
 import { getAccountsStrategys, accountStrategyListStringify } from '@/assets/scripts/actions/accountStrategyActions';
 const inquirer = require('inquirer');
 inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
@@ -11,30 +10,37 @@ export const monitPrompt = async (list: boolean) => {
         return monitor()
     };
     
-    // const { mds, tds, strategies } = await getAccountsStrategys();
-    // const accountStrategyList = accountStrategyListStringify(mds, tds, strategies);
-    // let answers = await inquirer.prompt([
-    //     {
-    //         type: 'autocomplete',
-    //         name: 'processName',
-    //         message: 'Select target account / strategy process to monit    ',
-    //         source: async (answersSoFar: any, input: string) => {
-    //             input = input || '';
-    //             return accountStrategyList
-    //                 .filter((s: string) => s.toLowerCase().includes('md'))
-    //                 .filter((s: string) => s.includes(input.trim()))
-    //         }
-    //     }
-    // ]);
+    const { mds, tds, strategies } = await getAccountsStrategys(); 
+    const accountStrategyList = accountStrategyListStringify(mds, tds, strategies);
 
-    // //it is so damn important, because inquirer event will conflict with blessed
-    // process.stdin.removeAllListeners('data'); 
 
-    // const { processName } = answers;
-    // const processSplit = processName.split(' ')
-    // const processId = processSplit[processSplit.length - 1].trim();
-    // const type = processSplit[0].includes('strategy') ? 'strategy' : 'account';
-    // return tradingData(processId, type)
+        
+    let answers = await inquirer.prompt([
+        {
+            type: 'autocomplete',
+            name: 'processName',
+            message: 'Select target account / strategy process to monit    ',
+            source: async (answersSoFar: any, input: string) => {
+                input = input || '';
+                return accountStrategyList
+                    .filter((s: string) => !s.includes('md'))
+                    .filter((s: string) => s.includes(input.trim()))
+            }
+        }
+    ]);
+
+
+    //it is so damn important, because inquirer event will conflict with blessed
+    process.stdin.removeAllListeners('data'); 
+
+    const { processName } = answers;
+    const processSplit = processName.split(' ')
+    const processId = processSplit[processSplit.length - 1].trim();
+    const type = processSplit[0].includes('strategy') ? 'strategy' : 'account';
+    process.env.CLI_WINDOW_TYPE = ['monit', type, processId].join('_');
+
+    const tradingData = require('@/components/tradingData/index').default
+    return tradingData(processId, type)
 }
 
 
