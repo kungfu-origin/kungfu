a3094d3bd8adfca178f304b9f17921f203504da9
swang <shuai.wang@taurus.ai>
2020-04-16 17:09:41 +0800

    [add] windows kill

--- core/node/index.js
+++ core/node/index.js
@@ -1,2 +1,4 @@
-const kungfu = require('./kungfu.js')
+const kungfu = require('./kungfu.js');
+const windowsKill = require('./windows-kill/index');
 exports.kungfu = kungfu;
+exports.windowsKill = windowsKill
