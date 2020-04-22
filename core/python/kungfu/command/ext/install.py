f4d73dd30feefa2ca86fb3c00b783a9123df8a9c
Keren Dong <dongkeren@gmail.com>
2020-04-22 22:19:21 +0800

    Fix ext commands

--- core/python/kungfu/command/ext/install.py
+++ core/python/kungfu/command/ext/install.py
@@ -16,8 +16,8 @@ def install(ctx, file):
     filename_re = re.match(r'kfext_(\w+)-v\d+\.\d+\.\d+\.tgz', filename)
     if filename_re:
         ext_name = filename_re.group(1)
-        install_path = os.path.join(extensions.extension_path, ext_name)
-        ext_cache_dir = os.path.join(extensions.extension_path, '.cache')
+        install_path = os.path.join(kungfu_extensions.extension_path, ext_name)
+        ext_cache_dir = os.path.join(kungfu_extensions.extension_path, '.cache')
         os.makedirs(ext_cache_dir, exist_ok=True)
         extfile = tarfile.open(file)
         if not extfile.getmember('package'):
--- core/python/kungfu/command/ext/list.py
+++ core/python/kungfu/command/ext/list.py
@@ -8,10 +8,10 @@ from kungfu.command.ext import ext as kfext, pass_ctx_from_parent
 def list(ctx):
     pass_ctx_from_parent(ctx)
     click.echo("Installed extensions:")
-    for ext_name in extensions.EXTENSIONS.keys():
+    for ext_name in kungfu_extensions.EXTENSIONS.keys():
         ext_funcs = []
-        if extensions.EXTENSION_REGISTRY_MD.has_extension(ext_name):
+        if kungfu_extensions.EXTENSION_REGISTRY_MD.has_extension(ext_name):
             ext_funcs.append('md')
-        if extensions.EXTENSION_REGISTRY_TD.has_extension(ext_name):
+        if kungfu_extensions.EXTENSION_REGISTRY_TD.has_extension(ext_name):
             ext_funcs.append('td')
         click.echo('%s %s' % (ext_name, ext_funcs))
