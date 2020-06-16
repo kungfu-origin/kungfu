1b8f084b98e69800c293619e97a30ac3d4a6436f
Keren Dong <dongkeren@gmail.com>
2020-06-16 14:51:38 +0800

    Clean up csv temp files

--- core/python/kungfu/data/adapter.py
+++ core/python/kungfu/data/adapter.py
@@ -44,25 +44,28 @@ class Adapter:
         sink = yjj.null_sink()
         writers = {}
         for csv_file in glob.glob(os.path.join(self.ctx.inbox_dir, '*.csv')):
-            filename = os.path.splitext(os.path.basename(csv_file))[0]
-            name_list = filename.split('.')
-            if len(name_list) != 4:
-                self.ctx.logger.error(f'invalid csv name {filename}')
-                continue
-            category, group, name, type_name = name_list
-            writer_key = category + group + name
-            if writer_key not in writers:
-                home = yjj.location(lf.enums.mode.DATA, lf.enums.get_category_by_name(category), group, name, output_locator)
-                writers[writer_key] = yjj.writer(home, 0, True, sink.publisher)
-            writer = writers[writer_key]
-            data_type = self.named_types[type_name]
-            with open(csv_file, 'r', newline='') as file:
-                reader = csv.reader(file)
-                header = next(reader)
-                for row in reader:
-                    json_text = '{' + ','.join([f'"{t[0]}":{t[1]}' for t in zip(header, row)]) + '}'
-                    data = data_type(json_text)
-                    writer.write_at(int(row[0]), 0, data)
+            try:
+                filename = os.path.splitext(os.path.basename(csv_file))[0]
+                name_list = filename.split('.')
+                if len(name_list) != 4:
+                    self.ctx.logger.error(f'invalid csv name {filename}')
+                    continue
+                category, group, name, type_name = name_list
+                writer_key = category + group + name
+                if writer_key not in writers:
+                    home = yjj.location(lf.enums.mode.DATA, lf.enums.get_category_by_name(category), group, name, output_locator)
+                    writers[writer_key] = yjj.writer(home, 0, True, sink.publisher)
+                writer = writers[writer_key]
+                data_type = self.named_types[type_name]
+                with open(csv_file, 'r', newline='') as file:
+                    reader = csv.reader(file)
+                    header = next(reader)
+                    for row in reader:
+                        json_text = '{' + ','.join([f'"{t[0]}":{t[1]}' for t in zip(header, row)]) + '}'
+                        data = data_type(json_text)
+                        writer.write_at(int(row[0]), 0, data)
+            finally:
+                os.remove(csv_file)
 
         yjj.assemble([output_locator, backup_locator]) >> yjj.copy_sink(target_locator)
 
