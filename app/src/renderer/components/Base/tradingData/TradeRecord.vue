530e8951deaeacafbb71b22d18560bbbe0b6f2eb
deltaZang <zhangyeezhi@gmail.com>
2020-07-01 15:45:47 +0800

    [fix] side width

--- app/src/renderer/components/Base/tradingData/TradeRecord.vue
+++ app/src/renderer/components/Base/tradingData/TradeRecord.vue
@@ -134,7 +134,7 @@ export default {
                 type: 'text',
                 label: '',
                 prop: 'side',
-                width: '35px'
+                width: '60px'
             },{
                 type: 'text',
                 label: '',
