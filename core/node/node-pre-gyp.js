df3d5fef988c3ea3c0691936cf0bf18d5c9ee5f4
Keren Dong <dongkeren@gmail.com>
2021-02-01 17:57:46 +0800

    Update build cmd

--- core/node/node-pre-gyp.js
+++ core/node/node-pre-gyp.js
@@ -1,27 +1,36 @@
 const {spawn} = require("child_process");
+
+function node_pre_gyp(cmd, argv) {
+    const build_type_arg = argv.buildType === "Debug" ? ["--debug"] : []
+    const yarn_args = ["node-pre-gyp", ...build_type_arg, ...cmd]
+
+    const install = spawn("yarn", yarn_args, {
+        shell: true,
+        stdio: ["inherit", "inherit", "inherit"],
+        windowsHide: true
+    });
+
+    install.on("error", (error) => {
+        process.stderr.write(error)
+    });
+
+    install.on("close", code => {
+        console.log(`yarn node-pre-gyp install exited with code ${code}`);
+    });
+}
+
 const argv = require("yargs/yargs")(process.argv.slice(2))
     .command("install", "install through node-pre-gyp", (yargs) => {
-        yargs.positional("build-type", {
-            choices: ["Release", "Debug"],
-            default: "Release"
-        })
     }, (argv) => {
-        const build_type_arg = argv.buildType === "Debug" ? ["--debug"] : []
-        const yarn_args = ["node-pre-gyp", "install", "--fallback-to-build", ...build_type_arg]
-
-        const install = spawn("yarn", yarn_args, {
-            shell: true,
-            stdio: ["inherit", "inherit", "inherit"],
-            windowsHide: true
-        });
-
-        install.on("error", (error) => {
-            process.stderr.write(error)
-        });
-
-        install.on("close", code => {
-            console.log(`yarn node-pre-gyp install exited with code ${code}`);
-        });
+        node_pre_gyp(["install", "--fallback-to-build"], argv)
+    })
+    .command("build", "build from source", (yargs) => {
+    }, (argv) => {
+        node_pre_gyp(["configure", "build", "package"], argv)
+    })
+    .option("build-type", {
+        choices: ["Release", "Debug"],
+        default: "Release"
     })
     .demandCommand()
     .help()
