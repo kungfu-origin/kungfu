1b78ffee61dd9a3a8ac1ed2e4daac31ae0096d7b
Keren Dong <dongkeren@gmail.com>
2021-02-03 17:54:31 +0800

    Update ci for unix

--- core/python/gyp-yarn.py
+++ core/python/gyp-yarn.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import subprocess
+import platform
 
-sys.stdout.write(f"node-gyp args: {sys.argv}")
+sys.stdout.write(f"python gyp args: {sys.argv}\n")
 sys.stdout.flush()
 
 cmd = os.path.basename(sys.argv[1])  # strip leading path for gyp on windows
-subprocess.run(["yarn", cmd], shell=True)
+subprocess.run(["yarn", cmd], shell=platform.system() == "Windows")
