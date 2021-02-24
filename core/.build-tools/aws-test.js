1444160e4ee0a1a932ec2b925c2acf43e2fd3f5a
Keren Dong <dongkeren@gmail.com>
2021-02-24 16:13:35 +0800

    Workaround traceroute for ubuntu

--- core/.build-tools/aws-test.js
+++ core/.build-tools/aws-test.js
@@ -1,12 +1,16 @@
 const {spawnSync} = require("child_process");
 const os = require("os");
 
+const spawnOpts = {shell: true, stdio: "inherit", windowsHide: true};
+
 require("yargs/yargs")(process.argv.slice(2))
     .command("traceroute", "traceroute to AWS S3", (yargs) => {
     }, (argv) => {
         const traceroute = os.platform() === "win32" ? "tracert" : "traceroute";
-        const result = spawnSync(traceroute, ["kungfu.s3.cn-northwest-1.amazonaws.com.cn"], {shell: true, stdio: "inherit", windowsHide: true});
-        process.exit(result.status);
+        if (os.platform() === "linux") {
+            spawnSync("apt-get", ["install", "-y", "traceroute"], spawnOpts);;
+        }
+        spawnSync(traceroute, ["kungfu.s3.cn-northwest-1.amazonaws.com.cn"], spawnOpts);
     })
     .demandCommand()
     .help()
