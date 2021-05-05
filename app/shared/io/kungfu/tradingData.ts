645f2c828603e031533c71c6e90392056079e2f7
ayizhi <zhangyiizhi@126.com>
2021-05-05 19:54:40 +0800

    [fix] before calc in a subscriber

--- app/shared/io/kungfu/tradingData.ts
+++ app/shared/io/kungfu/tradingData.ts
@@ -35,6 +35,48 @@ function ensureLeaderData (data: any, key = '') {
 
     return data ? data.sort(key) : []
 }
+
+export const buildAllTradingDataPipe = () => {
+    return KUNGFU_TRADING_DATA_OBSERVER.pipe(
+        map((data: any): any => {
+            const ledgerData = data.ledger || {};
+            const orderInputs = ensureLeaderData(ledgerData.OrderInput, 'insert_time').map((item: OrderInputOriginData) => dealOrderInput(item));
+            const orders = ensureLeaderData(ledgerData.Order, 'update_time').map((item: OrderOriginData) => dealOrder(item));
+            const trades = ensureLeaderData(ledgerData.Trade, 'trade_time').map((item: TradeOriginData) => dealTrade(item));
+            const positions = ensureLeaderData(ledgerData.Position).map((item: PosOriginData) => dealPos(item));
+            const assets = ensureLeaderData(ledgerData.Asset).map((item: AssetOriginData) => dealAsset(item));
+            const orderStat = ensureLeaderData(ledgerData.OrderStat).map((item: OrderStatOriginData) => dealOrderStat(item));
+            const pnl = ensureLeaderData(ledgerData.AssetSnapshot, 'update_time').map((item: AssetSnapshotOriginData) => dealSnapshot(item));
+            const dailyAsset = ensureLeaderData(ledgerData.DailyAsset, 'trading_day').map((item: AssetSnapshotOriginData) => dealSnapshot(item));
+            const instruments = ensureLeaderData(ledgerData.Instrument);
+            
+            return {
+                allOrders: orders,
+                allTrades: trades,
+                instruments: instruments,
+                orderStat: transformOrderStatListToData(orderStat),
+
+                ordersByAccount: transformOrderTradeListToData(orders, 'account'),
+                ordersByStrategy: transformOrderTradeListToData(orders, 'strategy'),
+                
+                tradsByAccount: transformOrderTradeListToData(trades, 'account'),
+                tradsByStrategy: transformOrderTradeListToData(trades, 'strategy'),
+                
+                positionsByAccount: transformTradingItemListToData(positions, 'account'),
+                positionsByStrategy: transformTradingItemListToData(positions, 'strategy'),
+                positionsByTicker: transformTradingItemListToData(positions, 'ticker'),
+                
+                assetsByAccount: transformAssetItemListToData(assets, 'account'),
+                assetsByStrategy: transformAssetItemListToData(assets, 'strategy'),
+
+                pnlByAccount: transformTradingItemListToData(pnl, 'account'),
+                pnlByStrategy: transformTradingItemListToData(pnl, 'strategy'),
+            }
+        })
+    
+        
+    )
+}
        
 export const buildTradingDataPipe = (type: string) => {
     return KUNGFU_TRADING_DATA_OBSERVER.pipe(
