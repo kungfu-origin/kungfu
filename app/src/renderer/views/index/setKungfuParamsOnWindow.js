c145d9dbde5ce268f0db24004041c8bb3ce9213a
ayizhi <zhangyiizhi@126.com>
2021-05-21 18:48:35 +0800

    [fix] history

--- app/src/renderer/views/index/setKungfuParamsOnWindow.js
+++ app/src/renderer/views/index/setKungfuParamsOnWindow.js
@@ -5,13 +5,14 @@ process.env.RENDERER_TYPE = 'app';
 
 
 // debug export
-const { kungfu, longfist, kungfuConfigStore } = require('__io/kungfu/kungfuUtils')
+const { kungfu, longfist, kungfuConfigStore, history } = require('__io/kungfu/kungfuUtils')
 const { watcher, startGetKungfuWathcerStep } = require('__io/kungfu/watcher');
 
 window.watcher = watcher;
 window.longfist = longfist;
 window.kungfu = kungfu;
 window.kungfuConfigStore = kungfuConfigStore;
+window.kungfuHistory = history;
 
 startGetKungfuWathcerStep();
 
