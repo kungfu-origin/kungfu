6ac95a630c9a5f97d6b3324084859c1f048c01b9
Keren DONG <keren.dong@kungfu.link>
2021-07-18 21:01:48 +0800

    Update copyright

--- framework/core/python/kungfu/__init__.py
+++ framework/core/python/kungfu/__init__.py
@@ -1,5 +1,5 @@
 '''
-Copyright [taurus.ai]
+Copyright [www.kungfu-trader.com]
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
