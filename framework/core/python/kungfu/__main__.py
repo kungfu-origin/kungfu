6bc2aa3cea76450ff0cbc889abdcff56b892715c
Keren DONG <keren.dong@kungfu.link>
2021-07-19 23:47:45 +0800

    Add nuitka config

--- framework/core/python/kungfu/__main__.py
+++ framework/core/python/kungfu/__main__.py
@@ -1,10 +1,35 @@
-# Environment variables that affects:
-#   KF_HOME - base folder for kungfu files
-#   KF_LOG_LEVEL - logging level
-#   KF_NO_EXT - disable extensions if set
-
+from kungfu.command import __all__ as commands
 import kungfu.command as kfc
-from kungfu.command import __all__
+
+# Nuitka Settings
+###########################################################
+# nuitka-project: --standalone
+# nuitka-project: --output-dir=../build
+# nuitka-project: --remove-output
+# nuitka-project: --assume-yes-for-downloads
+# nuitka-project: --prefer-source-code
+# nuitka-project: --static-libpython=yes
+# nuitka-project: --python-flag=nosite
+# nuitka-project: --python-flag=no_warnings
+# nuitka-project: --warn-unusual-code
+# nuitka-project: --warn-implicit-exceptions
+# nuitka-project: --include-package=numpy
+# nuitka-project: --include-package=pandas
+# nuitka-project: --include-package=plotly
+# nuitka-project: --include-package=wcwidth
+# nuitka-project: --include-package=recordclass
+# nuitka-project: --include-package=sortedcontainers
+# nuitka-project: --include-package=dotted_dict
+# nuitka-project: --include-package=yaml
+# nuitka-project: --enable-plugin=numpy
+###########################################################
+
+# Environment Variables
+###########################################################
+# KF_HOME - base folder for kungfu files
+# KF_LOG_LEVEL - logging level
+# KF_NO_EXT - disable extensions if set
+###########################################################
 
 
 def main():
@@ -12,4 +37,5 @@ def main():
 
 
 if __name__ == "__main__":
+    dir(commands)
     main()
