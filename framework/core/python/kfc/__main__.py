908795d12e583d92ba5e86df07f60c11612575b0
Keren DONG <keren.dong@kungfu.link>
2021-07-20 23:25:44 +0800

    Update nuitka

--- framework/core/python/kfc/__main__.py
+++ framework/core/python/kfc/__main__.py
@@ -1,14 +1,21 @@
 # Nuitka Settings
 ###########################################################
 # nuitka-project: --standalone
+#
 # nuitka-project: --output-dir=../build
 # nuitka-project: --remove-output
+#
+# nuitka-project: --include-data-file=../build/kfc/*kungfu*.*=/
+#
 # nuitka-project: --assume-yes-for-downloads
 # nuitka-project: --prefer-source-code
 # nuitka-project: --python-flag=nosite
 # nuitka-project: --python-flag=no_warnings
+# nuitka-project: --static-libpython=no
+#
 # nuitka-project: --warn-unusual-code
 # nuitka-project: --warn-implicit-exceptions
+#
 # nuitka-project: --include-package=numpy
 # nuitka-project: --include-package=pandas
 # nuitka-project: --include-package=plotly
@@ -17,10 +24,19 @@
 # nuitka-project: --include-package=sortedcontainers
 # nuitka-project: --include-package=dotted_dict
 # nuitka-project: --include-package=yaml
-# nuitka-project: --nofollow-import-to=pandas.tests
-# nuitka-project: --nofollow-import-to=numpy.distutils
+#
+# nuitka-project: --nofollow-import-to=*.distutils
+# nuitka-project: --nofollow-import-to=*.tests
+#
+# nuitka-project: --enable-plugin=anti-bloat
 # nuitka-project: --enable-plugin=numpy
 # nuitka-project: --enable-plugin=pylint-warnings
+#
+# nuitka-project: --noinclude-pytest-mode=nofollow
+# nuitka-project: --noinclude-setuptools-mode=nofollow
+# nuitka-project: --noinclude-custom-mode=distutils:nofollow
+#
+# nuitka-project: --no-progress
 ###########################################################
 
 from kungfu import __main__ as origin
