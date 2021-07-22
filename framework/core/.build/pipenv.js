acce1b59ccafaa50812f31f5039e9f029478fe78
Keren DONG <keren.dong@kungfu.link>
2021-07-22 10:53:02 +0800

    Fix pipenv install

--- framework/core/.build/pipenv.js
+++ framework/core/.build/pipenv.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
-const fse = require('fs-extra');
 const path = require('path');
 const { spawnSync } = require('child_process');
 
-const argv = require('yargs/yargs')(process.argv.slice(2)).option('python').option('pypi-mirror').help().argv;
+const argv = require('yargs/yargs')(process.argv.slice(2))
+  .option('clear', { type: 'boolean', default: false })
+  .option('python')
+  .option('pypi-mirror')
+  .help().argv;
 
-const lock_path = path.join('Pipfile.lock');
-const python_opt = argv.python && !fse.existsSync(lock_path) ? ['--python', argv.python] : [];
+const base_path = path.dirname(__dirname);
+const lock_path = path.join(base_path, 'Pipfile.lock');
+
+const python_opt = argv.clear && argv.python ? ['--python', argv.python] : [];
 const pypi_opt = argv.pypiMirror ? ['--pypi-mirror', argv.pypiMirror] : [];
 const pipenv_args = [...python_opt, ...pypi_opt, ...argv._];
 
@@ -14,13 +19,14 @@ console.log(`$ pipenv ${pipenv_args.join(' ')}`);
 
 const result = spawnSync('pipenv', pipenv_args, {
   shell: true,
-  stdio: ['inherit', 'inherit', 'inherit'],
+  stdio: 'inherit',
   windowsHide: true,
 });
 
 if (result.status === 0) {
-  const time = new Date();
-  fs.utimesSync(lock_path, time, time);
+  const atime = fs.lstatSync(lock_path).atime;
+  const mtime = new Date();
+  fs.utimesSync(lock_path, atime, mtime);
 }
 
 process.exit(result.status);
