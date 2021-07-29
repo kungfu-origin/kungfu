5c036e1a03609827c8dd87841306a3b570f14646
Keren DONG <keren.dong@kungfu.link>
2021-07-29 09:45:37 +0800

    Fix editor.cmake

--- framework/core/.cmake/editor.cmake
+++ framework/core/.cmake/editor.cmake
@@ -1,32 +1,32 @@
 # to corporate clion with cmake-js:
 # https://github.com/cmake-js/cmake-js/issues/23
-if(CLION)
-    function(PARSE_CMAKEJS_PROPERTIES)
-        function(GET_VARIABLE INPUT_STRING VARIABLE_TO_SELECT OUTPUT_VARIABLE)
-            set(SEARCH_STRING "${VARIABLE_TO_SELECT}=\"")
+if (CLION)
+    function(parse_cmakejs_properties)
+        function(get_variable INPUT_STRING VARIABLE_TO_SELECT OUTPUT_VARIABLE)
+            set(SEARCH_STRING "${VARIABLE_TO_SELECT}=")
             string(LENGTH "${SEARCH_STRING}" SEARCH_STRING_LENGTH)
             string(LENGTH "${INPUT_STRING}" INPUT_STRING_LENGTH)
 
-            string(FIND "${INPUT_STRING}" "${VARIABLE_TO_SELECT}=\"" SEARCH_STRING_INDEX)
+            string(FIND "${INPUT_STRING}" "${VARIABLE_TO_SELECT}=" SEARCH_STRING_INDEX)
 
             math(EXPR SEARCH_STRING_INDEX "${SEARCH_STRING_INDEX}+${SEARCH_STRING_LENGTH}")
 
             string(SUBSTRING "${INPUT_STRING}" ${SEARCH_STRING_INDEX} ${INPUT_STRING_LENGTH} AFTER_SEARCH_STRING)
-            string(FIND "${AFTER_SEARCH_STRING}" "\"" QUOTE_INDEX)
+            string(FIND "${AFTER_SEARCH_STRING}" "'" QUOTE_INDEX)
             string(SUBSTRING "${AFTER_SEARCH_STRING}" "0" "${QUOTE_INDEX}" RESULT_STRING)
             set("${OUTPUT_VARIABLE}" "${RESULT_STRING}" PARENT_SCOPE)
-        endfunction(GET_VARIABLE)
+        endfunction(get_variable)
 
-        if(WIN32)
+        if (WIN32)
             set(CMAKE_JS_CMD "./node_modules/.bin/cmake-js.cmd")
-        else()
+        else ()
             set(CMAKE_JS_CMD "./node_modules/.bin/cmake-js")
-        endif()
+        endif ()
 
         string(TOLOWER "${CMAKE_BUILD_TYPE}" CMAKE_BUILD_TYPE_LOWER)
         if (CMAKE_BUILD_TYPE_LOWER STREQUAL "debug")
             set(DEBUG_OPTION "--debug")
-        else()
+        else ()
             set(DEBUG_OPTION "")
         endif ()
 
@@ -41,8 +41,12 @@ if(CLION)
         get_variable("${CMAKE_JS_OUTPUT}" "CMAKE_LIBRARY_OUTPUT_DIRECTORY" CMAKE_LIBRARY_OUTPUT_DIRECTORY)
         set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}" PARENT_SCOPE)
 
-        message("-- Clion ready")
-    endfunction(PARSE_CMAKEJS_PROPERTIES)
+        if (CMAKE_JS_INC STREQUAL "")
+            message(FATAL_ERROR "Failed to get CMAKE_JS_INC")
+        endif ()
 
-    PARSE_CMAKEJS_PROPERTIES()
-endif()
+        message(STATUS "Clion ready")
+    endfunction(parse_cmakejs_properties)
+
+    parse_cmakejs_properties()
+endif ()
