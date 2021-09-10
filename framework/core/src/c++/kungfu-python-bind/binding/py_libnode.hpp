14d0a744e9ecf9df206894bd966a4f45d2601d5e
Keren DONG <keren.dong@kungfu.link>
2021-09-10 16:17:51 +0800

    Fix libnode call

--- framework/core/src/c++/kungfu-python-bind/binding/py_libnode.hpp
+++ framework/core/src/c++/kungfu-python-bind/binding/py_libnode.hpp
@@ -12,14 +12,31 @@
 namespace kungfu::libnode {
 namespace py = pybind11;
 
+inline size_t get_total_size(const py::args &args) {
+  size_t total_size = 0;
+  for (int i = 0; i < args.size(); i++) {
+    total_size += py::len(args[i]) + 1;
+  }
+  return total_size;
+}
+
 void run(const py::args &args, const py::kwargs &kwargs) {
-  int argc = static_cast<int>(args.size());
+  size_t argv_pos = 0;
+  size_t argc = args.size();
   char **argv = new char *[argc];
+  char *argv_buffer = new char[get_total_size(args)]; // argv must be allocated adjacently
+
   for (int i = 0; i < argc; i++) {
     std::string arg = args[i].cast<py::str>();
-    argv[i] = const_cast<char *>(arg.c_str());
+    argv[i] = argv_buffer + argv_pos;
+    strncpy(argv[i], arg.c_str(), arg.length());
+    argv[i][arg.length()] = '\0';
+    argv_pos += arg.length() + 1;
   }
-  node::Start(argc, argv);
+
+  node::Start(static_cast<int>(argc), argv);
+
+  delete[] argv_buffer;
   delete[] argv;
 }
 
