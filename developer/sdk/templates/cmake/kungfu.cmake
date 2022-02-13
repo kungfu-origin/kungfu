904c79a33fcf985e84bba0d804ec58191f0ba6c2
Keren DONG <keren.dong@kungfu.link>
2022-02-13 21:43:48 +0800

    Update kungfu.cmake

--- developer/sdk/templates/cmake/kungfu.cmake
+++ developer/sdk/templates/cmake/kungfu.cmake
@@ -1,46 +1,5 @@
 macro(kungfu_setup MODULE_NAME)
-  set(CMAKE_CXX_STANDARD 20)
-
-  if(UNIX)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0")
-    set(CMAKE_CXX_FLAGS_DEBUG "-g")
-    set(CMAKE_CXX_FLAGS_RELEASE "")
-
-    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE})
-    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE})
-    set(CMAKE_BUILD_WITH_INSTALL_RPATH ON)
-  endif()
-  if(UNIX AND NOT APPLE)
-    set(KFC_1INSTALL_RPATH
-            "$ORIGIN"
-            "$ORIGIN/../../"
-            )
-    set(CMAKE_INSTALL_RPATH "${KFC_INSTALL_RPATH}")
-  endif()
-  if(APPLE)
-    set(KFC_INSTALL_RPATH
-        "@loader_path"
-        "@loader_path/../../"
-        "@executable_path/../../../../Resources/kfc"
-        )
-    set(CMAKE_INSTALL_RPATH "${KFC_INSTALL_RPATH}")
-    set(CMAKE_MACOSX_RPATH ON)
-    set(CONAN_DISABLE_CHECK_COMPILER on)
-  endif()
-  if(WIN32)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /utf-8 /permissive- /bigobj /MP /W0")
-    set(CMAKE_GENERATOR_PLATFORM x64)
-    set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS OFF)
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /IGNORE:4199")
-    set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} /IGNORE:4199")
-    add_compile_definitions(HAVE_SNPRINTF)
-    add_compile_definitions(V8_DEPRECATION_WARNINGS=1)
-    add_compile_definitions(_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING)
-  endif()
-
-  if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftemplate-backtrace-limit=0 -Wno-address-of-packed-member -Wno-deprecated")
-  endif ()
+  include(<%- kfcDir -%>/cmake/compiler.cmake)
 
   add_compile_definitions(FMT_HEADER_ONLY)
 
