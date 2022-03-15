acebf4c7eb4181f3d406044e8f3d93d0eb065d0a
yizhi <zhangyiizhi@126.com>
2022-03-15 16:18:40 +0800

    [fix] prettier

--- framework/app/lib/webpack/webpack.renderer.config.js
+++ framework/app/lib/webpack/webpack.renderer.config.js
@@ -74,27 +74,7 @@ const webpackConfig = (argv) => {
         },
       ],
     },
-    plugins: [
-      ...pagesConfig.plugins,
-      new VueLoaderPlugin(),
-      // new CopyPlugin({
-      //   patterns: [
-      //     {
-      //       from: path.posix.join(
-      //         path.join(appDir, 'public').replace(/\\/g, '/'),
-      //         '**/*',
-      //       ),
-      //       to: path.join(argv.distDir, argv.distName),
-      //       filter: (resourcePath) => {
-      //         if (resourcePath.includes('vue-devtool')) {
-      //           return false;
-      //         }
-      //         return true;
-      //       },
-      //     },
-      //   ],
-      // }),
-    ],
+    plugins: [...pagesConfig.plugins, new VueLoaderPlugin()],
     target: 'electron-renderer',
   });
 };
