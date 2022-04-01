4b1c7ecfac70be8f75a2fadf61b9fa7fc0ffeb77
yizhi <zhangyiizhi@126.com>
2022-04-01 10:03:40 +0800

    [fix] core format

--- framework/core/.gyp/node-all-format.js
+++ framework/core/.gyp/node-all-format.js
@@ -1,11 +1,42 @@
+const path = require('path');
 const { spawnSync } = require('child_process');
+const moduleDir = path.resolve(
+  require.resolve('@kungfu-trader/kungfu-core'),
+  '..',
+  '..',
+);
 
-dealSpawnSyncResult(spawnSync('yarn', ['format:cpp']));
-dealSpawnSyncResult(spawnSync('yarn', ['format:python']));
-dealSpawnSyncResult(spawnSync('yarn', ['format:js']));
+dealSpawnSyncResult(
+  spawnSync('npm', ['run', 'format:cpp'], {
+    shell: true,
+    stdio: 'inherit',
+    windowsHide: true,
+    cwd: moduleDir,
+  }),
+);
+dealSpawnSyncResult(
+  spawnSync('npm', ['run', 'format:python'], {
+    shell: true,
+    stdio: 'inherit',
+    windowsHide: true,
+    cwd: moduleDir,
+  }),
+);
+dealSpawnSyncResult(
+  spawnSync('npm', ['run', 'format:js'], {
+    shell: true,
+    stdio: 'inherit',
+    windowsHide: true,
+    cwd: moduleDir,
+  }),
+);
 
 function dealSpawnSyncResult(res) {
-  const { stderr, stdout } = res;
+  const { stderr, stdout, error } = res;
+
+  if (error) {
+    console.error(error);
+  }
   if (stdout) {
     console.log(stdout.toString());
   }
