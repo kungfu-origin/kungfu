f0d65562e817a4337cf46e64053deb96b7dfcd4a
yizhi <zhangyiizhi@126.com>
2022-04-01 22:44:49 +0800

    [fix] details

--- framework/app/src/renderer/pages/code/components/FileTree.vue
+++ framework/app/src/renderer/pages/code/components/FileTree.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="file-tree">
-    <a-button type="primary" class="open-editor-folder" @click="handleBindStrategyFolder">设置策略入口文件</a-button>
+    <a-button
+      type="primary"
+      class="open-editor-folder"
+      @click="handleBindStrategyFolder"
+    >
+      设置策略入口文件
+    </a-button>
     <div class="file-tree-content">
       <div class="strategy-name">
         <span class="name">
@@ -8,35 +14,35 @@
           （当前策略)
         </span>
         <span class="tree-deal-file">
-             <span
-                class="create"
-                title="新建文件"
-                v-if="strategyPath"
-                @click="handleAddFile"
-            >
-                <FileAddFilled class="icon"/>
-            </span>
-            <span
-                class="create"
-                title="新建文件夹"
-                v-if="strategyPath"
-                @click="handleAddFolder"
-            >
-                <FolderAddFilled class="icon"/>
-            </span>
+          <span
+            class="create"
+            title="新建文件"
+            v-if="strategyPath"
+            @click="handleAddFile"
+          >
+            <FileAddFilled class="icon" />
+          </span>
+          <span
+            class="create"
+            title="新建文件夹"
+            v-if="strategyPath"
+            @click="handleAddFolder"
+          >
+            <FolderAddFilled class="icon" />
+          </span>
         </span>
       </div>
       <div class="file-tree-body" v-if="strategyPath">
         <div class="scroll-view">
-            <div v-for="file in fileTree" :key="`${file.id}_${file.fileId}`">
-                <FileNode
-                    v-if="file.root"
-                    :count="0"
-                    :fileNode="file"
-                    :id="file.id"
-                    type="folder"
-                ></FileNode>
-            </div>
+          <div v-for="file in fileTree" :key="`${file.id}_${file.fileId}`">
+            <FileNode
+              v-if="file.root"
+              :count="0"
+              :fileNode="file"
+              :id="file.id"
+              type="folder"
+            ></FileNode>
+          </div>
         </div>
       </div>
     </div>
@@ -45,271 +51,272 @@
 
 <script lang="ts">
 export default {
-    emits: ['updateStrategy']
-}
+  emits: ['updateStrategy'],
+};
 </script>
 <script setup lang="ts">
-import { defineProps, watch, ref, getCurrentInstance, ComponentInternalInstance, computed } from 'vue';
+import {
+  watch,
+  ref,
+  getCurrentInstance,
+  ComponentInternalInstance,
+  computed,
+} from 'vue';
 import path from 'path';
 import { storeToRefs } from 'pinia';
 import { dialog } from '@electron/remote';
 import { message } from 'ant-design-vue';
 import { getTreeByFilePath } from '../../../assets/methods/codeUtils';
-import { useCodeStore } from '../store/codeStore'
+import { useCodeStore } from '../store/codeStore';
 import FileNode from './FileNode.vue';
 import { updateStrategyPath } from '@kungfu-trader/kungfu-js-api/kungfu/strategy';
 import { findTargetFromArray } from '@kungfu-trader/kungfu-js-api/utils/busiUtils';
 import { openFolder, buildFileObj } from '../../../assets/methods/codeUtils';
 
-import { FileAddFilled, FolderAddFilled} from '@ant-design/icons-vue';
+import { FileAddFilled, FolderAddFilled } from '@ant-design/icons-vue';
 
 const store = useCodeStore();
-
 const props = defineProps<{
-    strategy: Code.Strategy
-   
-}>()
-const { strategy } = props
-const strategyPath = ref<string>('')
-const strategyPathName = ref<string>('')
-const { proxy } = getCurrentInstance() as ComponentInternalInstance
+  strategy: Code.Strategy;
+}>();
+const { strategy } = props;
+const strategyPath = ref<string>('');
+const strategyPathName = ref<string>('');
+const { proxy } = getCurrentInstance() as ComponentInternalInstance;
 const { currentFile } = storeToRefs(useCodeStore());
-const fileTree = computed(() => store.fileTree)
+const fileTree = computed(() => store.fileTree);
 
-watch(strategy as Code.Strategy, newStrategy => {
-    getPath(newStrategy);
-    initFileTree(newStrategy).then (fileTree => {
-        const entryPath: string = newStrategy.strategy_path
-        
-        const currentFile = findTargetFromArray<Code.FileData>(Object.values(fileTree), 'filePath' , entryPath)
-        
-        if (currentFile) {
-            store.setEntryFile(currentFile)
-            store.setCurrentFile(currentFile)
-        }       
-    })
-})
+watch(strategy as Code.Strategy, (newStrategy) => {
+  getPath(newStrategy);
+  initFileTree(newStrategy).then((fileTree) => {
+    const entryPath: string = newStrategy.strategy_path;
 
- //绑定策略
+    const currentFile = findTargetFromArray<Code.FileData>(
+      Object.values(fileTree),
+      'filePath',
+      entryPath,
+    );
+
+    if (currentFile) {
+      store.setEntryFile(currentFile);
+      store.setCurrentFile(currentFile);
+    }
+  });
+});
+
+//绑定策略
 function handleBindStrategyFolder() {
-    dialog.showOpenDialog(
-        {
-            properties: ['openFile'],
-        },
-    ).then (strategyPath => {
-            if (!strategyPath || !strategyPath.filePaths[0]) return;
-            if (!strategy?.strategy_id) return;
-            bindStrategyPath(strategyPath.filePaths[0]);
+  dialog
+    .showOpenDialog({
+      properties: ['openFile'],
+    })
+    .then((strategyPath) => {
+      if (!strategyPath || !strategyPath.filePaths[0]) return;
+      if (!strategy?.strategy_id) return;
+      bindStrategyPath(strategyPath.filePaths[0]);
     });
 }
 
 //bind data中path 与 sqlite中path
 async function bindStrategyPath(strategyPathNew) {
-    if (strategy && strategy.strategy_id) {
-        await updateStrategyPath(strategy.strategy_id, strategyPathNew)
-        
-        message.success(
-            `策略${strategy.strategy_id}文件路径修改成功！`,
-        );
-        //每次更新path，需要通知root组件更新stratgy
-        proxy?.$emit('updateStrategy', strategyPathNew);
-    }
+  if (strategy && strategy.strategy_id) {
+    await updateStrategyPath(strategy.strategy_id, strategyPathNew);
+
+    message.success(`策略${strategy.strategy_id}文件路径修改成功！`);
+    //每次更新path，需要通知root组件更新stratgy
+    proxy?.$emit('updateStrategy', strategyPathNew);
+  }
 }
 
 //加文件夹
 function handleAddFolder() {
-    let id: number = currentFile.value.id
-    let rootId: number = +store.getRootFileId
-    const target = fileTree.value[id] || fileTree.value[rootId]
-    if (target.isDir) {
-        openFolder(target, fileTree.value, true).then(() => {
-            store.addFileFolderPending({ id: target.id, type: 'folder' })
-            },
-        );
+  let id: number = currentFile.value.id;
+  let rootId: number = +store.getRootFileId;
+  const target = fileTree.value[id] || fileTree.value[rootId];
+  if (target.isDir) {
+    openFolder(target, fileTree.value, true).then(() => {
+      store.addFileFolderPending({ id: target.id, type: 'folder' });
+    });
+  } else {
+    if (target.parentId !== undefined && !isNaN(target.parentId)) {
+      store.addFileFolderPending({
+        id: target.parentId,
+        type: 'folder',
+      });
     } else {
-        if (target.parentId !== undefined && !isNaN(target.parentId)) {
-        store.addFileFolderPending({
-            id: target.parentId,
-            type: 'folder',
-        })
-        } else {
-            throw new Error();
-        }
+      throw new Error();
     }
+  }
 }
 
 //加文件
 function handleAddFile() {
-    let id = currentFile.value.id
-    let rootId: number = +store.getRootFileId
-    const target = fileTree.value[id] || fileTree.value[rootId]
-    if (target.isDir) {
-        openFolder(target, fileTree.value, true).then(() => {
-            store.addFileFolderPending({ id: target.id, type: '' })
-        },
-    );
+  let id = currentFile.value.id;
+  let rootId: number = +store.getRootFileId;
+  const target = fileTree.value[id] || fileTree.value[rootId];
+  if (target.isDir) {
+    openFolder(target, fileTree.value, true).then(() => {
+      store.addFileFolderPending({ id: target.id, type: '' });
+    });
+  } else {
+    if (target.parentId !== undefined && !isNaN(target.parentId)) {
+      store.addFileFolderPending({ id: target.parentId, type: '' });
     } else {
-        if (target.parentId !== undefined && !isNaN(target.parentId)) {
-            store.addFileFolderPending({ id: target.parentId, type: '' })
-        } else {
-            throw new Error();
-        }
+      throw new Error();
     }
+  }
 }
 
 //从prop内获取path
-function getPath (strategy: Code.Strategy) {
-    if (strategy && strategy.strategy_path) {
-        //因为绑定策略时是文件，需要提取其父目录
-        strategyPath.value = path.dirname(strategy.strategy_path);
-        strategyPathName.value = path.basename(strategyPath.value);
-    }
+function getPath(strategy: Code.Strategy) {
+  if (strategy && strategy.strategy_path) {
+    //因为绑定策略时是文件，需要提取其父目录
+    strategyPath.value = path.dirname(strategy.strategy_path);
+    strategyPathName.value = path.basename(strategyPath.value);
+  }
 }
 async function initFileTree(strategy) {
-    if (!strategy.strategy_id || !strategy.strategy_path) return;
-      //根文件夹得信息不像其他通过fs读取，而是直接从props的strategy中去取
-    const rootId = window.fileId++;
-    
-    
-    const rootFile: Code.FileData = buildFileObj({
-        id: rootId,
-        parentId: 0,
-        isDir: true,
-        name: strategyPathName.value,
-        ext: '',
-        filePath: strategyPath.value,
-        children: {file: [], folder: []},
-        stats: {},
-        root: true,
-        open: true,
-        fileId: 1
-    });
-    //获取第一级文件树
-    let ids, fileTree;
-    try {
-        const fileTreeData = await getTreeByFilePath(rootFile, fileTree);
-        ids = fileTreeData.ids;
-        fileTree = fileTreeData.fileTree
-    } catch (err) {
-        message.error(err);
-    }
+  if (!strategy.strategy_id || !strategy.strategy_path) return;
+  //根文件夹得信息不像其他通过fs读取，而是直接从props的strategy中去取
+  const rootId = window.fileId++;
 
-    // 处理根
-    rootFile['children'] = ids;
-    fileTree[rootId] = rootFile;
-    // padding
-    fileTree = bindFunctionalNode(fileTree);
-    store.setFileTree(fileTree);
-    store.setCurrentFile(rootFile)
-    
-    return fileTree
+  const rootFile: Code.FileData = buildFileObj({
+    id: rootId,
+    parentId: 0,
+    isDir: true,
+    name: strategyPathName.value,
+    ext: '',
+    filePath: strategyPath.value,
+    children: { file: [], folder: [] },
+    stats: {},
+    root: true,
+    open: true,
+    fileId: 1,
+  });
+  //获取第一级文件树
+  let ids, fileTree;
+  try {
+    const fileTreeData = await getTreeByFilePath(rootFile, fileTree);
+    ids = fileTreeData.ids;
+    fileTree = fileTreeData.fileTree;
+  } catch (err) {
+    message.error(err);
+  }
+
+  // 处理根
+  rootFile['children'] = ids;
+  fileTree[rootId] = rootFile;
+  // padding
+  fileTree = bindFunctionalNode(fileTree);
+  store.setFileTree(fileTree);
+  store.setCurrentFile(rootFile);
+
+  return fileTree;
 }
 
 //增加 以pending 为key 的node，为addfile做准备
 function bindFunctionalNode(fileTree) {
-    fileTree['pending'] = {
-        parentId: '',
-    };
-    return fileTree;
+  fileTree['pending'] = {
+    parentId: '',
+  };
+  return fileTree;
 }
-
 </script>
 
 <style lang="less">
 .file-tree {
-    width: 300px;
-    padding-top: 8px;
-    text-align: center;
+  width: 300px;
+  padding-top: 8px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  background: @component-background;
+
+  .open-editor-folder {
+    width: 90%;
+    height: 36px;
+    margin: auto;
+  }
+
+  .strategy-name {
+    font-size: 14px;
+    font-weight: bolder;
+    margin-top: 8px;
+    height: 30px;
+    line-height: 30px;
+    padding: 0px 8px;
+    border-top: none;
+    border-bottom: 2px solid @component-background;
+    text-align: left;
+    box-sizing: border-box;
+    color: @text-color;
+    .name {
+      span {
+        vertical-align: bottom;
+        display: inline-block;
+        max-width: 160px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+      }
+    }
+    .tree-deal-file {
+      display: none;
+    }
+    .folder-oper {
+      cursor: pointer;
+      display: none;
+      padding: 0 6px;
+      i {
+        font-size: 15px;
+      }
+    }
+  }
+  .folder-name {
+    height: 30px;
+    line-height: 30px;
+    color: @text-color;
+    text-align: left;
     display: flex;
-    flex-direction: column;
-    background: @component-background;
-
-    .open-editor-folder {
-        width: 90%;
-        height: 36px;
-        margin: auto;
+    flex-direction: row;
+    padding-left: 5px;
+    .name {
+      max-width: 100px;
+      font-size: 14px;
+      padding-right: 20px;
+      color: @text-color;
     }
-    
-    .strategy-name {
-        font-size: 14px;
-        font-weight: bolder;
-        margin-top: 8px;
-        height: 30px;
-        line-height: 30px;
-        padding: 0px 8px;
-        border-top: none;
-        border-bottom: 2px solid @component-background;
-        text-align: left;
-        box-sizing: border-box;
-        color: @text-color;
-        .name {
-            span {
-                vertical-align: bottom;
-                display: inline-block;
-                max-width: 160px;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-                overflow: hidden;
-            }
-        }
-        .tree-deal-file {
-            display: none;
-        }
-        .folder-oper {
-            cursor: pointer;
-            display: none;
-            padding: 0 6px;
-            i {
-                font-size: 15px;
-            }
-        }
+    .title {
+      color: @input-bg;
     }
-    .folder-name {
-        height: 30px;
-        line-height: 30px;
-        color: @text-color;
-        text-align: left;
-        display: flex;
-        flex-direction: row;
-        padding-left: 5px;
-        .name {
-            max-width: 100px;
-            font-size: 14px;
-            padding-right: 20px;
-            color: @text-color;
-        }
-        .title {
-            color: @input-bg;
-        }
+  }
+  .file-tree-content {
+    height: calc(100% - 40px);
+  }
+  .file-tree-body {
+    height: calc(100% - 38px);
+    overflow: auto;
+    padding-left: 5px;
+    .scroll-view {
+      overflow-y: scroll;
     }
-    .file-tree-content {
-        height: calc(100% - 40px);
-    }
-    .file-tree-body {
-        height: calc(100% - 38px);
-        overflow: auto;
-        padding-left: 5px;
-        .scroll-view {
-            overflow-y: scroll;
-
+  }
+  &:hover {
+    .tree-deal-file {
+      display: block;
+      float: right;
+      margin-right: 10px;
+      .create {
+        margin: 0 2px;
+        cursor: pointer;
+        .icon {
+          color: @text-color;
+          &:hover {
+            color: @icon-color-hover;
+          }
         }
+      }
     }
-    &:hover {
-        .tree-deal-file {
-            display: block;
-            float: right;
-            margin-right: 10px;
-            .create {
-                margin: 0 2px;
-                cursor: pointer;
-                .icon {
-                    color: @text-color;
-                    &:hover {
-                        color: @icon-color-hover;
-                    }
-                }
-            }
-        }
-    }
+  }
 }
 </style>
--- framework/app/src/renderer/pages/code/components/MonacoEditor.vue
+++ framework/app/src/renderer/pages/code/components/MonacoEditor.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="code-editor">
-      <div id="editor-content" v-if="file !== null && !file.isDir"></div>
-      <i class="iconfont tr-logo" v-else></i>
+    <div id="editor-content" v-if="file !== null && !file.isDir"></div>
+    <i class="iconfont tr-logo" v-else></i>
   </div>
 </template>
 <script setup lang="ts">
 import { findTargetFromArray } from '@kungfu-trader/kungfu-js-api/utils/busiUtils';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.main.js';
 import { storeToRefs } from 'pinia';
-import { computed, nextTick, reactive, watch } from 'vue'
+import { computed, nextTick, reactive, watch } from 'vue';
 import languageJSON from '../config/iconFileConfig.json';
 import themeData from '../config/Monocai.json';
 import {
@@ -17,11 +17,11 @@ import {
   kungfuProperties,
   kungfuKeywords,
   pythonKeywords,
- } from '../hint/monaco.python.hint';
+} from '../hint/monaco-python-hint';
 import { useCodeStore } from '../store/codeStore';
 import { getCodeText } from '@kungfu-trader/kungfu-js-api/utils/fileUtils';
 import path from 'path';
-import fse from 'fs-extra'
+import fse from 'fs-extra';
 
 monaco.editor.defineTheme('monokai', themeData);
 monaco.editor.setTheme('monokai');
@@ -30,143 +30,151 @@ monaco.languages.registerCompletionItemProvider('python', {
   provideCompletionItems: pythonProvideCompletionItems,
 });
 
-const store = useCodeStore();
-
 // currentFile
-const { currentFile, fileTree, kfConfig } = storeToRefs(store)
-const code = computed(() => kfConfig['code'])
+const { currentFile, fileTree, kfConfig } = storeToRefs(useCodeStore());
+const code = computed(() => kfConfig['code']);
 
+let handleEditor: monaco.editor.IDiffNavigator = reactive({});
+let file: Code.FileProps = reactive({});
 
-let handleEditor: monaco.editor.IDiffNavigator = reactive({})
-let file: Code.FileProps = reactive({})
-
-
-
-watch(code, spaceTabSetting => {
-    updateSpaceTab(spaceTabSetting || {})
-    
-})
-
+watch(code, (spaceTabSetting) => {
+  updateSpaceTab(spaceTabSetting || {});
+});
 
 // 监听文件树变化
 watch(fileTree, (newTree, oldTree) => {
-    let newRootPath = findTargetFromArray<Code.FileData>(Object.values(newTree), 'root', true)!.filePath
-    
-    let oldRootPath = ''
-    if (oldTree[0] && oldTree[0].id) {
-        oldRootPath = findTargetFromArray<Code.FileData>(Object.values(oldTree), 'root', true)!.filePath
-    }
-    if (newRootPath !== oldRootPath) {
-        file = {};
-        handleEditor = null
-    }
-})
+  let newRootPath = findTargetFromArray<Code.FileData>(
+    Object.values(newTree),
+    'root',
+    true,
+  )!.filePath;
+
+  let oldRootPath = '';
+  if (oldTree[0] && oldTree[0].id) {
+    oldRootPath = findTargetFromArray<Code.FileData>(
+      Object.values(oldTree),
+      'root',
+      true,
+    )!.filePath;
+  }
+  if (newRootPath !== oldRootPath) {
+    file = {};
+    handleEditor = null;
+  }
+});
 
 // 监听当前文件状态
 watch(currentFile, async (newFile: Code.FileProps) => {
-    const filePath: string = newFile.filePath || '';
+  const filePath: string = newFile.filePath || '';
 
-    if (currentFile.value.isDir) return;
-    clearState();
-    file = newFile;
-    const codeText: string = await getCodeText(filePath)
-    await nextTick();
-    handleEditor = buildEditor(handleEditor, file, codeText);
-    await nextTick();
-    // updateSpaceTab(code.value);
-    bindBlur(handleEditor, file);
-})
+  if (currentFile.value.isDir) return;
+  clearState();
+  file = newFile;
+  const codeText: string = await getCodeText(filePath);
+  await nextTick();
+  handleEditor = buildEditor(handleEditor, file, codeText);
+  await nextTick();
+  // updateSpaceTab(code.value);
+  bindBlur(handleEditor, file);
+});
 
 function bindBlur(editor, file) {
-    editor !== null &&
+  editor !== null &&
     editor.onDidBlurEditorText(() => {
-        curWriteFile(editor, file);
+      curWriteFile(editor, file);
     });
 }
 
 function curWriteFile(editor, file) {
-    const value = editor.getValue();
-    let curPath: string = path.normalize(file.filePath)
-    fse.outputFile(curPath, value);
+  const value = editor.getValue();
+  let curPath: string = path.normalize(file.filePath);
+  fse.outputFile(curPath, value);
 }
 
 // 创建代码编辑器
 function createEditor(file: Code.FileProps, codeText: string): monaco.editor {
-    if (document.getElementById('editor-content')) {
-        (document.getElementById('editor-content') as any).innerHTML = '';
-        let fileLanguage: string = 'plaintext';
-        if (file) {
-            fileLanguage = file.ext ? languageJSON[file.ext] : 'plaintext';
-        }
-        const editor: monaco.editor = monaco.editor.create(
-            document.getElementById("editor-content"),
-            {
-                value: codeText,
-                language: fileLanguage,
-
-                autoIndent: 'full',
-                formatOnPaste: true,
-                formatOnType: true,
-
-                fontSize: 14,
-                automaticLayout: true,
-            }
-        );
-        return editor;
+  if (document.getElementById('editor-content')) {
+    (document.getElementById('editor-content') as any).innerHTML = '';
+    let fileLanguage: string = 'plaintext';
+    if (file) {
+      fileLanguage = file.ext ? languageJSON[file.ext] : 'plaintext';
     }
-    return null
+    const editor: monaco.editor = monaco.editor.create(
+      document.getElementById('editor-content'),
+      {
+        value: codeText,
+        language: fileLanguage,
+
+        autoIndent: 'full',
+        formatOnPaste: true,
+        formatOnType: true,
+
+        fontSize: 14,
+        automaticLayout: true,
+      },
+    );
+    return editor;
+  }
+  return null;
 }
 
 // 更新代码编辑器
-function updateEditor(editor: monaco.editor, file: Code.FileProps, codeText: string): monaco.editor {
-    editor.updateOptions({value: codeText});
-    const fileLanguage = file.ext ? (languageJSON[file.ext] || 'plaintext') : 'plaintext';
-    handleEditor.setModelLanguage(editor.getModel(), fileLanguage);
-    return editor;
+function updateEditor(
+  editor: monaco.editor,
+  file: Code.FileProps,
+  codeText: string,
+): monaco.editor {
+  editor.updateOptions({ value: codeText });
+  const fileLanguage = file.ext
+    ? languageJSON[file.ext] || 'plaintext'
+    : 'plaintext';
+  handleEditor.setModelLanguage(editor.getModel(), fileLanguage);
+  return editor;
 }
 
 // 构建代码编辑器
-function buildEditor(editor: monaco.editor, file: Code.FileProps, codeText: string) {
-    if (!editor) {
-        return createEditor(file, codeText);
-    } else {
-        return updateEditor(handleEditor, file, codeText);
-    }
+function buildEditor(
+  editor: monaco.editor,
+  file: Code.FileProps,
+  codeText: string,
+) {
+  if (!editor) {
+    return createEditor(file, codeText);
+  } else {
+    return updateEditor(handleEditor, file, codeText);
+  }
 }
-
 
 // 更新缩进设置
 function updateSpaceTab(spaceTabSetting: Code.ICodeSetting) {
-    const type: string = spaceTabSetting ? spaceTabSetting.tabSpaceType || 'spaces' : 'spaces'
+  const type: string = spaceTabSetting
+    ? spaceTabSetting.tabSpaceType || 'spaces'
+    : 'spaces';
 
-    if (handleEditor) {
-        const model = handleEditor.getModel();
-        if (type.toLowerCase() === 'spaces') {
-            model.updateOptions({
-                insertSpaces: true,
-                indentSize: spaceTabSetting.tabSpaceSize,
-                tabSize: spaceTabSetting.tabSpaceSize,
-            });
-        } else if (type.toLowerCase() === 'tabs') {
-            model.updateOptions({
-                insertSpaces: false,
-                indentSize: spaceTabSetting.tabSpaceSize,
-                tabSize: spaceTabSetting.tabSpaceSize,
-            });
-        }
+  if (handleEditor) {
+    const model = handleEditor.getModel();
+    if (type.toLowerCase() === 'spaces') {
+      model.updateOptions({
+        insertSpaces: true,
+        indentSize: spaceTabSetting.tabSpaceSize,
+        tabSize: spaceTabSetting.tabSpaceSize,
+      });
+    } else if (type.toLowerCase() === 'tabs') {
+      model.updateOptions({
+        insertSpaces: false,
+        indentSize: spaceTabSetting.tabSpaceSize,
+        tabSize: spaceTabSetting.tabSpaceSize,
+      });
     }
+  }
 }
-
-
-
 
 // 使用完成销毁实例
 function clearState(): void {
-    handleEditor && handleEditor.dispose();
-    handleEditor = null;
-    file = {};
+  handleEditor && handleEditor.dispose();
+  handleEditor = null;
+  file = {};
 }
-
 
 function pythonProvideCompletionItems(model, position, context, token) {
   const lastChars = model.getValueInRange({
@@ -220,19 +228,17 @@ function pythonProvideCompletionItems(model, position, context, token) {
     suggestions: [...pythonKeywords, ...kungfuKeywords, ...allCharList],
   };
 }
-
 </script>
 <style lang="less">
 .code-editor {
-    .monaco-editor {
-        height: 100vh !important;
-    }
+  .monaco-editor {
+    height: 100vh !important;
+  }
 }
 </style>
 <style lang="less" scoped>
-
 .code-editor {
-    width: 100%;
-    height: 100vh;
+  width: 100%;
+  height: 100vh;
 }
 </style>
