a7f4ad809bbb5cb523c0d70ec42a9e35b1e5f4fe
yizhi <zhangyiizhi@126.com>
2022-04-14 22:36:49 +0800

    [fix] cached book in watcher

--- framework/core/src/bindings/node/binding/watcher.cpp
+++ framework/core/src/bindings/node/binding/watcher.cpp
@@ -417,6 +417,10 @@ location_ptr Watcher::FindLocation(const Napi::CallbackInfo &info) {
 }
 
 void Watcher::InspectChannel(int64_t trigger_time, const Channel &channel) {
+  if (channel.source_id == cached_home_location_->uid or channel.dest_id == cached_home_location_->uid) {
+    return;
+  }
+
   if (channel.source_id != get_live_home_uid() and channel.dest_id != get_live_home_uid()) {
     reader_->join(get_location(channel.source_id), channel.dest_id, trigger_time);
   }
