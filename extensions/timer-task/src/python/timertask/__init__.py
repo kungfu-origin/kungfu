ced6c5081a98b3ba0b8c3f1aa246c2ae21a2f925
szr <songzhouran@qq.com>
2022-04-15 16:09:30 +0800

    [fix] add min_volume

--- extensions/timer-task/src/python/timertask/__init__.py
+++ extensions/timer-task/src/python/timertask/__init__.py
@@ -11,9 +11,6 @@ class orderTask:
         self.vol = vol
         self.side = side
         self.offset = offset
-    # def __str__(self):  # 定义打印对象时打印的字符串
-    #     return " ".join(str(item) for item in (
-    #         self.vol,self.side,self.offset,self.price))
 
 def pre_start(context):
     args = {}
@@ -28,9 +25,9 @@ def pre_start(context):
     context.volume_to_fill = 0
     context.steps_to_fill = 0
     context.VOLUME = 0
-    context.ORDERS = {} 
+    context.orders = {} 
     context.has_quote = False
-    context.min_vol = int(0)
+    context.MIN_VOL = int(0)
     context.log.info("arguments {}".format(context.arguments))
     argslist = context.arguments.split(';')
     for i, arg in enumerate(argslist):
@@ -74,7 +71,6 @@ def str_to_nanotime(tm) :
         year_month_day = time.strftime("%Y-%m-%d", time.localtime())
         ymdhms = year_month_day + ' ' + tm.split(' ')[1]
         timeArray = time.strptime(ymdhms, "%Y-%m-%d %H:%M:%S")
-        print(timeArray)
         nano = int(time.mktime(timeArray) * 10**9)
         return nano
 
@@ -82,18 +78,23 @@ def type_to_minvol(argument):
     switcher = {
         InstrumentType.Stock : int(100),
         InstrumentType.Future : int(1),
+        InstrumentType.Bond : int(1),
+        InstrumentType.StockOption : int(1),
+        InstrumentType.Fund : int(1),
+        InstrumentType.TechStock : int(100),
+        InstrumentType.Index : int(1),
     }
     return switcher.get(argument, int(1))
 
 def on_order(context, order):
     context.log.info("[on_order 0] {}".format(order))
     if order.status == OrderStatus.Filled :
-        del context.ORDERS[order.order_id]
+        del context.orders[order.order_id]
     elif (order.status == OrderStatus.Cancelled) or (order.status == OrderStatus.PartialFilledNotActive):
-        context.log.info("[on_order 1] {}".format(context.ORDERS))
-        del context.ORDERS[order.order_id]
-        context.log.info("[on_order 2] {}".format(context.ORDERS))
-        if (not context.ORDERS) and context.steps_to_fill > 1 :
+        context.log.info("[on_order 1] {}".format(context.orders))
+        del context.orders[order.order_id]
+        context.log.info("[on_order 2] {}".format(context.orders))
+        if (not context.orders) and context.steps_to_fill > 1 :
             orders_to_insert = make_plan(context, False)
             context.log.info("------------orders_to_insert---------------- total {} orders".format(len(orders_to_insert)))
             for item in orders_to_insert:
@@ -101,11 +102,11 @@ def on_order(context, order):
             make_order(context, orders_to_insert)
             context.steps_to_fill = context.steps_to_fill - 1
     elif (order.status == OrderStatus.Error) :
-        del context.ORDERS[order.order_id] 
+        del context.orders[order.order_id] 
 
 def on_trade(context, trade):
     context.log.info("[on_trade] {}".format(trade))
-    context.ORDERS[trade.order_id] -= trade.volume
+    context.orders[trade.order_id] -= trade.volume
     context.volume_to_fill -= trade.volume
 
 def print_datatime(context, info, nano) :
@@ -118,11 +119,11 @@ def on_quote(context, quote):
     context.ask_price = quote.ask_price[0]
     context.UPPER_LIMIT_PRICE = quote.upper_limit_price
     context.LOWER_LIMIT_PRICE = quote.lower_limit_price
-    if context.min_vol == 0 :
+    if context.MIN_VOL == 0 :
         context.log.info("instrument_id {} bid {} ask {} up {} down {}".format(quote.instrument_id, str(quote.bid_price)[1:-1], str(quote.ask_price)[1:-1], quote.upper_limit_price, quote.lower_limit_price))
-        context.min_vol = type_to_minvol(quote.instrument_type)
-        context.MAX_LOT_BY_STEP = int(math.floor(context.MAX_LOT_BY_STEP / float(context.min_vol)) * context.min_vol)
-        context.log.info("quote.instrument_type {} context.min_vol {} context.MAX_LOT_BY_STEP {}".format(quote.instrument_type, context.min_vol, context.MAX_LOT_BY_STEP))
+        context.MIN_VOL = type_to_minvol(quote.instrument_type)
+        context.MAX_LOT_BY_STEP = int(math.floor(context.MAX_LOT_BY_STEP / float(context.MIN_VOL)) * context.MIN_VOL)
+        context.log.info("quote.instrument_type {} context.MIN_VOL {} context.MAX_LOT_BY_STEP {}".format(quote.instrument_type, context.MIN_VOL, context.MAX_LOT_BY_STEP))
         assert context.MAX_LOT_BY_STEP > 0
 
     if not context.has_quote :
@@ -148,12 +149,9 @@ def on_quote(context, quote):
                 print_datatime(context, "add_timer for insert_order_task", context.START_TIME_IN_NANO + interval * i) 
         context.has_quote = True
 
-
-
-
 def insert_order_task(context, last_order):
     print_datatime(context, "execute insert_order_task", context.now())
-    if (not last_order) and context.ORDERS:
+    if (not last_order) and context.orders:
         cancel_all_orders(context)
     else :
         orders_to_insert = make_plan(context, last_order)
@@ -164,16 +162,16 @@ def insert_order_task(context, last_order):
         context.steps_to_fill = context.steps_to_fill - 1
 
 def cancel_all_orders(context):
-    context.log.info("[cancel_all_orders] {}".format(context.ORDERS))
-    for item in context.ORDERS.keys():
+    context.log.info("[cancel_all_orders] {}".format(context.orders))
+    for item in context.orders.keys():
         context.log.info(f"canecl order id {item}")
         context.cancel_order(item)
     
 def make_order(context, orders):
     for item in orders:
         order_id = context.insert_order(context.TICKER, context.EXCHANGE, context.ACCOUNT, item.price, item.vol, PriceType.Limit, item.side, item.offset)
-        context.ORDERS[order_id] = item.vol
-    context.log.info("[make_order] context.ORDERS {}".format(context.ORDERS))
+        context.orders[order_id] = item.vol
+    context.log.info("[make_order] context.orders {}".format(context.orders))
 
 def split_order(context, vol, side, offset, price, task_list):
     place_order_vol = int(0)
@@ -191,22 +189,22 @@ def calc_order_vol(context, pos, side, offset, price) :
     task_list = []
     offset_vol = (pos.volume - pos.yesterday_volume) if offset == Offset.CloseToday else pos.yesterday_volume
     offset_tag = Offset.CloseToday if offset == Offset.CloseToday else  Offset.CloseYesterday
-    if(offset_vol >= context.volume_to_fill/context.steps_to_fill):
-        vol = int(math.floor(context.volume_to_fill/context.steps_to_fill/float(context.min_vol)) * context.min_vol)
+    if(offset_vol >= context.volume_to_fill / context.steps_to_fill):
+        vol = int(math.floor(context.volume_to_fill / context.steps_to_fill / float(context.MIN_VOL)) * context.MIN_VOL)
         split_order(context, vol, side, offset_tag, price, task_list)
-    elif(pos.volume >= context.volume_to_fill/context.steps_to_fill):
+    elif(pos.volume >= context.volume_to_fill / context.steps_to_fill):
         if(offset_vol > 0):
-            vol = int(math.floor(offset_vol/float(context.min_vol)) * context.min_vol)
+            vol = int(math.floor(offset_vol / float(context.MIN_VOL)) * context.MIN_VOL)
             place_order_vol = split_order(context, vol, side, offset_tag, price, task_list)
-        vol = int(math.floor((context.volume_to_fill/context.steps_to_fill - place_order_vol)/float(context.min_vol)) * context.min_vol)
+        vol = int(math.floor((context.volume_to_fill / context.steps_to_fill - place_order_vol) / float(context.MIN_VOL)) * context.MIN_VOL)
         split_order(context, vol, side, Offset.Close, price, task_list)
     else:
         if(offset_vol > 0):
-            vol = int(math.floor(offset_vol/float(context.min_vol)) * context.min_vol)
+            vol = int(math.floor(offset_vol / float(context.MIN_VOL)) * context.MIN_VOL)
             place_order_vol = split_order(context, vol, side, offset_tag, price, task_list)
-        vol = int(math.floor((pos.volume - place_order_vol)/float(context.min_vol)) * context.min_vol)
+        vol = int(math.floor((pos.volume - place_order_vol) / float(context.MIN_VOL)) * context.MIN_VOL)
         place_order_vol += split_order(context, vol, side, Offset.Close, price, task_list)
-        vol = int(math.floor((context.volume_to_fill/context.steps_to_fill - place_order_vol)/float(context.min_vol)) * context.min_vol)
+        vol = int(math.floor((context.volume_to_fill / context.steps_to_fill - place_order_vol) / float(context.MIN_VOL)) * context.MIN_VOL)
         split_order(context, vol, side, Offset.Open, price, task_list)
     return task_list
 
@@ -215,20 +213,20 @@ def make_plan(context, last_order):
     vol = int(0)
     if last_order :
         pending_vol = int(0)
-        for order_vol in context.ORDERS.values():
+        for order_vol in context.orders.values():
             pending_vol += order_vol
-        order_price = context.UPPER_LIMIT_PRICE if context.SIDE==Side.Buy else context.LOWER_LIMIT_PRICE
+        order_price = context.UPPER_LIMIT_PRICE if context.SIDE == Side.Buy else context.LOWER_LIMIT_PRICE
         context.volume_to_fill -=  pending_vol
     else :
-        order_price = context.bid_price if context.SIDE==Side.Buy else context.ask_price
+        order_price = context.bid_price if context.SIDE == Side.Buy else context.ask_price
     if(context.OFFSET == Offset.Open):
-        vol = int(math.floor(context.volume_to_fill/context.steps_to_fill/float(context.min_vol)) * context.min_vol)
+        vol = int(math.floor(context.volume_to_fill / context.steps_to_fill / float(context.MIN_VOL)) * context.MIN_VOL)
         context.log.info(f"make_plan..................vol {vol}")
         task_list = []
         split_order(context, vol, context.SIDE, Offset.Open, order_price, task_list)
         return task_list
     elif(context.OFFSET == Offset.Close or context.OFFSET == Offset.CloseYesterday):
-        if(context.SIDE==Side.Buy):#Close, CloseYesterday, Side.Buy
+        if(context.SIDE == Side.Buy):#Close, CloseYesterday, Side.Buy
             for key in book.short_positions:
                 pos = book.short_positions[key]
                 if(pos.instrument_id == context.TICKER):
@@ -239,7 +237,7 @@ def make_plan(context, last_order):
                 if(pos.instrument_id == context.TICKER):
                     return calc_order_vol(context, pos, Side.Sell, Offset.CloseYesterday, order_price)
     else: # CloseToday, Side.Buy
-        if(context.SIDE==Side.Buy):
+        if(context.SIDE == Side.Buy):
             for key in book.short_positions:
                 pos = book.short_positions[key]
                 if(pos.instrument_id == context.TICKER):
