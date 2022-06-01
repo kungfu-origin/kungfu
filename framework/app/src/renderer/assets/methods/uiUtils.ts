763b8940d6dc0537bd7dc8ff04c73a48ecb3a94b
WANFQICHAO1998 <wangqichao1998@126.com>
2022-06-01 23:20:45 +0800

    [fix] change name

--- framework/app/src/renderer/assets/methods/uiUtils.ts
+++ framework/app/src/renderer/assets/methods/uiUtils.ts
@@ -264,7 +264,7 @@ export const openCodeView = (
   return openNewBrowserWindow('code', `?processId=${processId}`);
 };
 
-export const openTradingView = (): Promise<Electron.BrowserWindow> => {
+export const openTradingTaskView = (): Promise<Electron.BrowserWindow> => {
   return openNewBrowserWindow('trading', `?processId=tradingTask`);
 };
 
@@ -379,12 +379,13 @@ export const handleOpenCodeView = (
   });
 };
 
-export const handleOpenTradingView = (): Promise<Electron.BrowserWindow> => {
-  const openMessage = message.loading(t('open_code_editor'));
-  return openTradingView().finally(() => {
-    openMessage();
-  });
-};
+export const handleOpenTradingTaskView =
+  (): Promise<Electron.BrowserWindow> => {
+    const openMessage = message.loading(t('open_code_editor'));
+    return openTradingTaskView().finally(() => {
+      openMessage();
+    });
+  };
 
 export const useDashboardBodySize = (): {
   dashboardBodyHeight: Ref;
