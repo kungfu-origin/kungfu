1f355808ccea42896ba6479cf43c07f49dc1de87
NoTwoBoy <1244476905@qq.com>
2022-06-17 10:57:05 +0800

    [update] change startMd() params cwd & script

--- framework/api/src/utils/processUtils.ts
+++ framework/api/src/utils/processUtils.ts
@@ -644,6 +644,8 @@ export const startMd = async (sourceId: string): Promise<Proc | void> => {
   );
   return startProcess({
     name: `md_${sourceId}`,
+    cwd: path.join(KF_RUNTIME_DIR, 'md', sourceId, sourceId),
+    script: path.join(KFC_DIR, kfcName),
     args,
     max_restarts: 3,
     autorestart: true,
