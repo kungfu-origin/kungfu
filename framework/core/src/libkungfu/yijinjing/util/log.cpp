732800ed0087449ea0913c1e051dd719dcc3e8ec
Keren DONG <keren.dong@kungfu.link>
2022-06-24 18:20:42 +0800

    [upd] merge python logger into spdlog

--- framework/core/src/libkungfu/yijinjing/util/log.cpp
+++ framework/core/src/libkungfu/yijinjing/util/log.cpp
@@ -30,6 +30,21 @@ private:
   spdlog::pattern_formatter spdlog_formatter;
 };
 
+class emitable_logger : public spdlog::logger {
+public:
+  emitable_logger(std::string name, spdlog::sink_ptr single_sink)
+      : spdlog::logger(std::move(name), std::move(single_sink)) {}
+
+  emitable_logger(std::string name, spdlog::sinks_init_list sinks) : spdlog::logger(std::move(name), sinks) {}
+
+  void emit(const std::string &source_file, int &source_line, const std::string &funcname,
+            const std::string &logger_name, int log_level, const std::string &msg) {
+    spdlog::source_loc source_loc(source_file.c_str(), source_line, funcname.c_str());
+    spdlog::details::log_msg record(source_loc, logger_name, static_cast<spdlog::level::level_enum>(log_level), msg);
+    sink_it_(record);
+  }
+};
+
 spdlog::level::level_enum get_env_log_level(const data::locator_ptr &locator) {
   auto level_name = locator->has_env(LOG_LEVEL_ENV) ? locator->get_env(LOG_LEVEL_ENV) : DEFAULT_LOG_LEVEL_NAME;
   return spdlog::level::from_str(level_name);
@@ -39,26 +54,36 @@ std::shared_ptr<spdlog::logger> get_main_logger() { return spdlog::default_logge
 
 const std::string &setup_log(const data::location_ptr &location, const std::string &name) {
   if (spdlog::default_logger()->name().empty()) {
-    std::shared_ptr<spdlog::logger> logger;
+    std::shared_ptr<emitable_logger> logger;
     std::string log_file = location->locator->layout_file(location, longfist::enums::layout::LOG, name);
     auto daily_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(log_file, 0, 0);
 
     if (location->group != "node") {
       auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
       spdlog::sinks_init_list log_sinks = {console_sink, daily_sink};
-      logger = std::make_shared<spdlog::logger>(name, log_sinks);
+      logger = std::make_shared<emitable_logger>(name, log_sinks);
     } else {
-      logger = std::make_shared<spdlog::logger>(name, daily_sink);
+      logger = std::make_shared<emitable_logger>(name, daily_sink);
     }
 
     logger->set_formatter(spdlog::details::make_unique<pattern_formatter>());
     logger->set_level(get_env_log_level(location->locator));
     logger->flush_on(spdlog::level::trace);
 
-    spdlog::set_default_logger(logger);
+    spdlog::set_default_logger(std::static_pointer_cast<spdlog::logger>(logger));
   } else {
     SPDLOG_WARN("Setup log for {} more than once", name);
   }
   return name;
+}
+
+void emit_log(const std::string &source_file, int &source_line, const std::string &funcname,
+              const std::string &logger_name, int log_level, const std::string &msg) {
+  try {
+    auto logger = std::dynamic_pointer_cast<emitable_logger>(get_main_logger());
+    logger->emit(source_file, source_line, funcname, logger_name, log_level, msg);
+  } catch (const std::exception &e) {
+    SPDLOG_ERROR("Failed to emit log: {}", e.what());
+  }
 }
 } // namespace kungfu::yijinjing::log
--- framework/core/src/python/kungfu/yijinjing/log.py
+++ framework/core/src/python/kungfu/yijinjing/log.py
@@ -1,14 +1,7 @@
 import logging
 import kungfu
-import sys
-import platform
-from kungfu.yijinjing.time import *
 
-lf = kungfu.__binding__.longfist
 yjj = kungfu.__binding__.yijinjing
-
-LOG_MSG_FORMAT = "[%(nanotime)s] [%(loglevel)s] [%(process)6d/%(tid)-6d] [%(filename)s:%(lineno)d#%(funcName)s] %(message)s"
-LOG_FILE_DATEEXT_FORMAT = "%Y-%m-%d"
 
 LOG_LEVELS = {
     "trace": logging.DEBUG,
@@ -20,117 +13,31 @@ LOG_LEVELS = {
     "critical": logging.CRITICAL,
 }
 
-
-class ansicolor:
-    # Formatting codes
-    reset = "\033[m"
-    bold = "\033[1m"
-    dark = "\033[2m"
-    underline = "\033[4m"
-    blink = "\033[5m"
-    reverse = "\033[7m"
-    concealed = "\033[8m"
-    clear_line = "\033[K"
-    # Foreground colors
-    black = "\033[30m"
-    red = "\033[31m"
-    green = "\033[32m"
-    yellow = "\033[33m"
-    blue = "\033[34m"
-    magenta = "\033[35m"
-    cyan = "\033[36m"
-    white = "\033[37m"
-    # Background colors
-    on_black = "\033[40m"
-    on_red = "\033[41m"
-    on_green = "\033[42m"
-    on_yellow = "\033[43m"
-    on_blue = "\033[44m"
-    on_magenta = "\033[45m"
-    on_cyan = "\033[46m"
-    on_white = "\033[47m"
-
-
-COLORS = {
-    "debug": ansicolor.cyan,
-    "info": ansicolor.green,
-    "warn": ansicolor.yellow + ansicolor.bold,
-    "warning": ansicolor.yellow + ansicolor.bold,
-    "error": ansicolor.red + ansicolor.bold,
-    "critical": ansicolor.bold + ansicolor.on_red,
+SPDLOG_LOG_LEVELS = {
+    logging.DEBUG: 1,
+    logging.INFO: 2,
+    logging.WARNING: 3,
+    logging.ERROR: 4,
+    logging.CRITICAL: 5,
 }
 
 
-class KungfuFormatter(logging.Formatter):
-    def format_level(self, levelname):
-        return "{:^8}".format(levelname)
-
-    def format(self, record):
-        record.loglevel = self.format_level(record.levelname.lower())
-        record.nanotime = strfnow(format="%m/%d %H:%M:%S.%N")
-        record.tid = yjj.thread_id()
-        return logging.Formatter.format(self, record)
-
-
-class ColorFormatter(KungfuFormatter):
-    def format_level(self, levelname):
-        levelname_f = KungfuFormatter.format_level(self, levelname)
-        if yjj.in_color_terminal():
-            return "{}{}{}".format(COLORS[levelname], levelname_f, ansicolor.reset)
-        else:
-            return levelname_f
-
-
-class UnixConsoleHandler(logging.StreamHandler):
-    def __init__(self):
-        logging.StreamHandler.__init__(self, sys.stdout)
-        self.setFormatter(ColorFormatter(LOG_MSG_FORMAT))
-
-
-class WinConsoleHandler(logging.StreamHandler):
-    def __init__(self):
-        logging.StreamHandler.__init__(
-            self, open(sys.stdout.fileno(), mode="w", encoding="utf8")
-        )
-        self.setFormatter(KungfuFormatter(LOG_MSG_FORMAT))
-
+class SpdlogHandler(logging.StreamHandler):
     def emit(self, record):
-        try:
-            msg = self.format(record)
-            stream = self.stream
-            if yjj.in_color_terminal():
-                stream.write(msg[:28])
-                self.flush()
-                yjj.color_print(
-                    record.levelname.lower(), "{:^8}".format(record.levelname.lower())
-                )
-                stream.write(msg[36:])
-                stream.write(self.terminator)
-                self.flush()
-            else:
-                stream.write(msg)
-                stream.write(self.terminator)
-                self.flush()
-        except Exception:
-            self.handleError(record)
+        yjj.emit_log(
+            record.filename,
+            record.lineno,
+            record.funcName,
+            record.name,
+            SPDLOG_LOG_LEVELS[record.levelno],
+            record.msg,
+        )
 
 
-def create_logger(name, level, location):
-    logger = logging.getLogger(name)
-
+def create_logger(name, level, location=None):
     if location is not None:
-        log_dateext = strfnow(LOG_FILE_DATEEXT_FORMAT)
-        log_name = "{}_py_{}".format(name, log_dateext)
-        log_path = location.locator.layout_file(location, lf.enums.layout.LOG, log_name)
-
-        file_handler = logging.FileHandler(log_path)
-        file_handler.setFormatter(KungfuFormatter(LOG_MSG_FORMAT))
-        logger.addHandler(file_handler)
-
-    console_handler = (
-        WinConsoleHandler() if platform.system() == "Windows" else UnixConsoleHandler()
-    )
-    logger.addHandler(console_handler)
-
+        yjj.setup_log(location, name)
+    logger = logging.getLogger(name)
+    logger.addHandler(SpdlogHandler())
     logger.setLevel(LOG_LEVELS[level])
     return logger
