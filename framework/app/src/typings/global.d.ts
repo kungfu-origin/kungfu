194fbc742a7fda6cb5aa01c139c81cbad4c5b37e
yizhi <zhangyiizhi@126.com>
2022-07-09 22:30:48 +0800

    [fix] renderer id to string

--- framework/app/src/typings/global.d.ts
+++ framework/app/src/typings/global.d.ts
@@ -33,13 +33,7 @@ declare global {
       LANG_ENV: 'zh-CN' | 'en-US' | 'zh-HK' | undefined;
       APP_TYPE: 'cli' | 'renderer' | 'component' | 'daemon' | 'main';
       DAEMON_ID: string;
-      RENDERER_ID:
-        | 'app'
-        | 'admin'
-        | 'logview'
-        | 'makeOrder'
-        | 'codeEditor'
-        | 'dzxy';
+      RENDERER_ID: string;
       EXTENSION_DIRS: string;
       KFC_DIR: string;
       ELECTRON_RUN_AS_NODE: boolean;
