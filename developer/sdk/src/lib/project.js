d8d45babe5589a2dbb9d05dfc6140562ac92941b
yizhi <zhangyiizhi@126.com>
2022-07-14 10:37:56 +0800

    [fix] put copy .node to package.js

--- developer/sdk/src/lib/project.js
+++ developer/sdk/src/lib/project.js
@@ -1,6 +1,12 @@
 const findWorkspaceRoot = require('find-yarn-workspace-root');
 const fse = require('fs-extra');
 const path = require('path');
+const { glob } = require('glob');
+const { getPackageJson } = require('./extension');
+
+const pypackages = '__pypackages__';
+const kungfulibs = '__kungfulibs__';
+const kungfuLibDirPattern = path.join(kungfulibs, '*', '*');
 
 exports.configure = (writePackageJson = false, writeWorkflows = true) => {
   const packageJsonPath = path.join(process.cwd(), 'package.json');
@@ -24,5 +30,33 @@ exports.configure = (writePackageJson = false, writeWorkflows = true) => {
 };
 
 exports.package = () => {
+  const packageJson = getPackageJson();
+  const extensionName = packageJson.kungfuConfig.key;
+  const buildTargetDir = path.join('build', 'target');
+  const outputDir = path.join('dist', extensionName);
+
+  const copyOutput = (pattern) => {
+    glob.sync(pattern).forEach((p) => {
+      fse.copyFile(p, path.join(outputDir, path.basename(p)));
+    });
+  };
+
+  if (fse.existsSync(buildTargetDir)) {
+    copyOutput(path.join(buildTargetDir, '*'));
+  }
+
+  if (fse.existsSync(kungfulibs)) {
+    copyOutput(path.join(kungfuLibDirPattern, 'lib', '*'));
+  }
+
+  if (fse.existsSync(pypackages)) {
+    fse.copySync(pypackages, path.join(outputDir, pypackages));
+  }
+
+  fse.copySync(
+    require.resolve('@kungfu-trader/kungfu-core/dist/kfc/drone.node'),
+    path.join(outputDir, `${packageJson.binary.module_name}.node`),
+    {},
+  );
   require('@kungfu-trader/kungfu-core').prebuilt('package');
 };
