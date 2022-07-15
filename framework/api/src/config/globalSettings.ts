1ae60b2899855c05ef5e0522e2ee3821a9a9772a
yizhi <zhangyiizhi@126.com>
2022-07-15 14:47:23 +0800

    [fix] rocket disable

--- framework/api/src/config/globalSettings.ts
+++ framework/api/src/config/globalSettings.ts
@@ -1,4 +1,5 @@
 import fse from 'fs-extra';
+import os from 'os';
 import { SpaceSizeSettingEnum, SpaceTabSettingEnum } from '../typings/enums';
 import { KF_CONFIG_PATH, PY_WHL_DIR } from './pathConfig';
 import { CodeSizeSetting, CodeTabSetting } from './tradingConfig';
@@ -8,6 +9,7 @@ import {
 } from '@kungfu-trader/kungfu-js-api/language';
 import VueI18n from '@kungfu-trader/kungfu-js-api/language';
 const { t } = VueI18n.global;
+const numCPUs = os.cpus() ? os.cpus().length : 1;
 
 export interface KfSystemConfig {
   key: string;
@@ -54,6 +56,7 @@ export const getKfGlobalSettings = (): KfSystemConfig[] => [
         name: t('globalSettingConfig.rocket_model'),
         tip: t('globalSettingConfig.rocket_model_desc'),
         default: false,
+        disabled: numCPUs <= 4,
         type: 'bool',
       },
       {
