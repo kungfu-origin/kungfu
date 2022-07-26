9f790aa333ffa1659f5435e2afd2614480e4490c
Keren DONG <keren.dong@kungfu.link>
2022-07-26 15:49:16 +0800

    [upd] run-pipenv

--- framework/core/.gyp/run-pipenv.js
+++ framework/core/.gyp/run-pipenv.js
@@ -1,28 +1,27 @@
 const { shell } = require('../lib');
 
-const cli = require('sywac')
-  // options
-  .option('bare', { type: 'boolean', default: false })
-  .help('--help')
-  .version('--version');
-
-async function main() {
-  const argv = await cli.parseAndExit();
-
-  const bare_opt = argv.bare ? ['--bare'] : [];
-  const python_opt = argv.bare
-    ? ['--python', process.env.npm_package_config_python_version]
-    : [];
-  const pypi_opt = [
-    '--pypi-mirror',
-    process.env.npm_package_config_pypi_mirror,
-  ];
-  const pipenv_args = [...bare_opt, ...python_opt, ...pypi_opt, ...argv._];
-
-  shell.run('pipenv', pipenv_args);
-}
-
-module.exports.cli = cli;
-module.exports.main = main;
-
-if (require.main === module) main().catch(shell.exitOnError);
+module.exports = require('../lib/sywac')(
+  module,
+  (cli) => {
+    cli
+      .option('-q, --quiet', { type: 'boolean', default: false })
+      .command('*', {
+        setup: (sywac) => {
+          sywac.strict(false);
+        },
+        run: (argv) => {
+          const pythonOpt = argv.bare
+            ? ['--python', process.env.npm_package_config_python_version]
+            : [];
+          const pypiOpt = [
+            '--pypi-mirror',
+            process.env.npm_package_config_pypi_mirror,
+          ];
+          const cliArgs = process.argv.slice(2);
+          const pipenvArgs = [...pythonOpt, ...pypiOpt, ...cliArgs];
+          shell.run('pipenv', pipenvArgs);
+        },
+      });
+  },
+  { silent: true, help: false, version: false },
+);
