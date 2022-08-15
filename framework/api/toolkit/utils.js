1101f389fa2ddd7d906d50215430b48a9b2e13c3
yizhi <zhangyiizhi@126.com>
2022-08-15 18:51:51 +0800

    [add] deal space func

--- framework/api/toolkit/utils.js
+++ framework/api/toolkit/utils.js
@@ -231,3 +231,8 @@ exports.findPackageRoot = () => {
   }
   return findRoot(path.resolve(cwd));
 };
+
+exports.dealSpaceInPath = (pathname) => {
+  const normalizePath = path.normalize(pathname);
+  return normalizePath.replace(/ /g, ' ');
+};
