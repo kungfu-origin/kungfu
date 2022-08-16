05cdc43920d42a13bbffbdbe823a36cb4b32a12c
NoTwoBoy <1244476905@qq.com>
2022-08-16 17:07:28 +0800

    [update] remove dead-code。

--- framework/app/src/components/modules/makeOrder/MakeOrder.vue
+++ framework/app/src/components/modules/makeOrder/MakeOrder.vue
@@ -165,14 +165,9 @@ const makeOrderData = computed(() => {
 });
 
 const availTradingTaskExtensionList = computed(() => {
-  return [
-    ...getExtConfigList(extConfigs.value, 'strategy').filter(
-      (item) => uiExtConfigs.value[item.key]?.position === 'make_order',
-    ),
-    { name: 12456 },
-    { name: 12456 },
-    { name: 12456 },
-  ];
+  return getExtConfigList(extConfigs.value, 'strategy').filter(
+    (item) => uiExtConfigs.value[item.key]?.position === 'make_order',
+  );
 });
 
 const getResolvedOffset = (
