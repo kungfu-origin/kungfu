25309353b2d2f599d7637d33ca169b9b76b30c40
NoTwoBoy <1244476905@qq.com>
2022-09-09 15:55:02 +0800

    [update] fix some ui problems.

--- framework/cli/src/commanders/list.ts
+++ framework/cli/src/commanders/list.ts
@@ -1,26 +1,42 @@
 import { getAllKfConfigOriginData } from '@kungfu-trader/kungfu-js-api/actions';
-import { getIdByKfLocation } from '@kungfu-trader/kungfu-js-api/utils/busiUtils';
+import {
+  getIdByKfLocation,
+  getKfExtensionConfig,
+} from '@kungfu-trader/kungfu-js-api/utils/busiUtils';
 import colors from 'colors';
-import { parseToString } from '../assets/methods/utils';
+import { dealKfConfigValue, parseToString } from '../assets/methods/utils';
 
 export const listKfLocations = async (): Promise<void> => {
   const { md, td, strategy } = await getAllKfConfigOriginData();
+  const extConfigs = await getKfExtensionConfig();
 
-  const mdList = md.map((m): string => {
-    return parseToString(
-      [colors.yellow('Md'), colors.bold(getIdByKfLocation(m)), m.value],
-      [10, 15, 'auto'],
-      1,
-    );
-  });
+  const mdList = await Promise.all(
+    md.map(async (m): Promise<string> => {
+      return parseToString(
+        [
+          colors.yellow('Md'),
+          colors.bold(getIdByKfLocation(m)),
+          await dealKfConfigValue(m, extConfigs),
+        ],
+        [10, 15, 'auto'],
+        1,
+      );
+    }),
+  );
 
-  const tdList = td.map((t): string => {
-    return parseToString(
-      [colors.cyan('Td'), colors.bold(getIdByKfLocation(t)), t.value],
-      [10, 15, 'auto'],
-      1,
-    );
-  });
+  const tdList = await Promise.all(
+    td.map(async (t): Promise<string> => {
+      return parseToString(
+        [
+          colors.cyan('Td'),
+          colors.bold(getIdByKfLocation(t)),
+          await dealKfConfigValue(t, extConfigs),
+        ],
+        [10, 15, 'auto'],
+        1,
+      );
+    }),
+  );
   const strategyList = strategy.map((s): string => {
     return parseToString(
       [
