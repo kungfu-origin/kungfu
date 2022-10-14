2c6c4f9e21443d145eba6049b4f87f48cb686f6a
yizhi <zhangyiizhi@126.com>
2022-10-14 10:23:56 +0800

    [fix] opt strategy start

--- framework/core/src/bindings/node/binding/watcher.h
+++ framework/core/src/bindings/node/binding/watcher.h
@@ -258,19 +258,7 @@ private:
     return instruction.*id_ptr;
   }
 
-  template <typename DataType>
-  std::enable_if_t<not (std::is_same_v<DataType, longfist::types::Instrument> or std::is_same_v<DataType, longfist::types::Position>)>
-  UpdateLedger(const boost::hana::basic_type<DataType> &type) {
-    for (auto &pair : data_bank_[type]) {
-      auto &state = pair.second;
-      update_ledger(state.update_time, state.source, state.dest, state.data);
-    }
-  }
-
-  template <typename DataType>
-  std::enable_if_t<std::is_same_v<DataType, longfist::types::Instrument> or
-                   std::is_same_v<DataType, longfist::types::Position>>
-  UpdateLedger(const boost::hana::basic_type<DataType> &type) {
+  template <typename DataType> void UpdateLedger(const boost::hana::basic_type<DataType> &type) {
     using DataTypeMap = std::unordered_map<uint64_t, state<DataType>>;
     auto &target_map = const_cast<DataTypeMap &>(data_bank_[type]);
     auto iter = target_map.begin();
