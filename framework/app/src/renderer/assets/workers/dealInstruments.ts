caef34cf1359d73601def8fb8b15df05c9a5cbe1
NoTwoBoy <1244476905@qq.com>
2022-12-09 16:07:01 +0800

    [update] some changes.

--- framework/app/src/renderer/assets/workers/dealInstruments.ts
+++ framework/app/src/renderer/assets/workers/dealInstruments.ts
@@ -4,16 +4,32 @@ import iconv from 'iconv-lite';
 import { KF_INSTRUMENTS_PATH } from '@kungfu-trader/kungfu-js-api/config/pathConfig';
 import { InstrumentTypeEnum } from '@kungfu-trader/kungfu-js-api/typings/enums';
 
-function decodeBuffer(name: number[]) {
-  name = name.filter((n) => !!n);
-  const bufferFrom = Buffer.from(name as unknown as ArrayBuffer);
-  return isBufferGBK(bufferFrom)
-    ? iconv.decode(bufferFrom, 'gbk')
-    : iconv.decode(bufferFrom, 'utf8');
+const defaultCharset = 'utf8';
+
+const charsetTransformMap = {
+  'UTF-8': 'utf8',
+  GBK: 'gbk',
+  GB2312: 'gb2312',
+  GB18030: 'gb18030',
+  Big5: 'big5',
+};
+
+function getBufferCharset(buffer: Buffer) {
+  const { encoding, confidence } = jschardet.detect(buffer);
+  console.log(encoding, confidence);
+  if (confidence < 0.8) return defaultCharset;
+
+  return encoding in charsetTransformMap
+    ? charsetTransformMap[encoding]
+    : encoding;
 }
 
-function isBufferGBK(bufferFrom: Buffer) {
-  return jschardet.detect(bufferFrom).encoding !== 'UTF-8';
+function decodeBuffer(name: number[]) {
+  // name = name.filter((n) => !!n);
+  const bufferFrom = Buffer.from(name as unknown as ArrayBuffer);
+  const charset = getBufferCharset(bufferFrom);
+  console.log(charset);
+  return iconv.decode(bufferFrom, charset);
 }
 
 type InstrumentResolvedData = Record<string, KungfuApi.InstrumentResolved>;
@@ -22,11 +38,12 @@ const resolveInstruments = (
   existedInstruments: InstrumentResolvedData,
   instruments: KungfuApi.Instrument[],
 ): InstrumentResolvedData => {
-  return (instruments || []).reduce((existedData, item) => {
+  return (instruments || []).slice(10, 20).reduce((existedData, item) => {
     const { instrument_id, instrument_type, product_id, exchange_id, ukey } =
       item;
     const oldInstrument = existedData[ukey] || null;
     const instrumentName = decodeBuffer(product_id);
+    console.log(instrumentName);
     const instrumentNameResolved =
       instrumentName || oldInstrument?.instrumentName || '';
     const instrumentType =
