55d726beb1d8da13c9cb2ed59e622c8a53903b7b
yizhi <zhangyiizhi@126.com>
2022-12-13 10:44:10 +0800

    [add] init

---
+++ framework/core/src/include/kungfu/wingchun/basketorder/basketorder.h
@@ -0,0 +1,28 @@
+#ifndef WINGCHUN_BASKETORDER_H
+#define WINGCHUN_BASKETORDER_H
+
+#include <kungfu/longfist/longfist.h>
+#include <kungfu/wingchun/common.h>
+
+namespace kungfu::wingchun::basketorder {
+
+FORWORD_DECLARE_STRUCT_PTR(BasketOrder)
+FORWORD_DECLARE_CLASS_PTR(BasketOrderKeeper)
+
+// key = order_id
+typedef std::unordered_map<uint64_t, longfist::types::Order> OrderMap;
+
+// key = trade_id
+typedef std::unordered_map<uint64_t, longfist::types::Trade> TradeMap;
+
+struct BasketOrder {
+  uint64_t basket_order_id;
+    int64_t volume_total;
+    double asset_total;
+  OrderMap orders = {};
+  TradeMap trades = {};
+}
+
+}; // namespace kungfu::wingchun::basketorder
+
+#endif // WINGCHUN_BASKETORDER_H
