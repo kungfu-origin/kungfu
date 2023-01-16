ec0b069f5e2d7e7bc36a87c621bf5b923def528a
yizhi <zhangyiizhi@126.com>
2023-01-16 17:34:24 +0800

    [fix] api export

--- framework/api/src/index.ts
+++ framework/api/src/index.ts
@@ -1,5 +1,6 @@
 export * as config from './config/exports';
 export * as actions from './actions/exports';
 export * as kungfu from './kungfu/exports';
+export * as hooks from './hooks/index';
 export * as utils from './utils/exports';
 export * as language from './language';
