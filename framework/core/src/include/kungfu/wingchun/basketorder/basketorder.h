65fea65484f10d2c33457e54ea17215ea8f3b183
yizhi <zhangyiizhi@126.com>
2023-01-18 10:27:36 +0800

    [fix] format

--- framework/core/src/include/kungfu/wingchun/basketorder/basketorder.h
+++ framework/core/src/include/kungfu/wingchun/basketorder/basketorder.h
@@ -13,6 +13,57 @@ FORWARD_DECLARE_CLASS_PTR(BasketOrderEngine)
 // key = order_id
 typedef std::unordered_map<uint64_t, longfist::types::Order> OrderMap;
 
+inline int64_t get_merged_total_volume(const OrderMap &orders) {
+  std::unordered_map<uint32_t, int64_t> long_hash_to_total_merge_volume = {};
+  std::unordered_map<uint32_t, int64_t> short_hash_to_total_merge_volume = {};
+  for (auto &iter : orders) {
+    auto &order = iter.second;
+    auto instrument_key = hash_instrument(order.exchange_id, order.instrument_id);
+    auto direction = get_direction(order.instrument_type, order.side, order.offset);
+    auto &hash_to_total_merge_volume = long_hash_to_total_merge_volume;
+    if (direction == longfist::enums::Direction::Short) {
+      hash_to_total_merge_volume = short_hash_to_total_merge_volume;
+    }
+    hash_to_total_merge_volume.insert_or_assign(instrument_key,
+                                                std::max(hash_to_total_merge_volume[instrument_key], order.volume));
+  }
+
+  int64_t total_volume = 0;
+  for (auto &iter : long_hash_to_total_merge_volume) {
+    total_volume += iter.second;
+  }
+  for (auto &iter : short_hash_to_total_merge_volume) {
+    total_volume += iter.second;
+  }
+  return total_volume;
+}
+
+inline int64_t get_total_volume(const OrderMap &orders) {
+  int64_t volume = 0;
+  for (auto &iter : orders) {
+    volume += iter.second.volume;
+  }
+  return volume;
+}
+
+inline int64_t get_total_traded_volume(const OrderMap &orders) {
+  int64_t volume = 0;
+  for (auto &iter : orders) {
+    auto &order = iter.second;
+    volume += order.volume - order.volume_left;
+  }
+  return volume;
+}
+
+inline bool is_all_order_end(const OrderMap &orders) {
+  for (auto &iter : orders) {
+    if (not is_final_status(iter.second.status)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 struct BasketOrderState {
   kungfu::state<longfist::types::BasketOrder> state_data;
   OrderMap orders = {};
@@ -23,14 +74,6 @@ struct BasketOrderState {
   void update(const longfist::types::Order &order);
 
   kungfu::state<longfist::types::BasketOrder> &get_state();
-
-  bool is_all_order_end();
-
-  bool is_all_order_filled();
-
-  int64_t get_total_volume();
-
-  int64_t get_total_volume_left();
 };
 
 } // namespace kungfu::wingchun::basketorder
