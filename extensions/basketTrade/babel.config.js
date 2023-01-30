6c29da31e3d5c276b23ccf563cd6d706a4b71b80
NoTwoBoy <1244476905@qq.com>
2023-01-30 19:44:29 +0800

    [update] remove basketTrade in main repo.

--- extensions/basketTrade/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    ['import', { libraryName: 'ant-design-vue', style: true }],
-    'syntax-dynamic-import',
-  ],
-};
