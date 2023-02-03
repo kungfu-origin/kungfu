6f9285714b0a8e22bdbfa7d175fe94e596440436
yuying <ying.yu@kungfu.link>
2023-02-03 20:42:16 +0800

    calculte basket order volume

--- framework/core/src/include/kungfu/wingchun/basketorder/basketorder.h
+++ framework/core/src/include/kungfu/wingchun/basketorder/basketorder.h
@@ -39,37 +39,40 @@ inline bool is_all_order_end(const OrderMap &orders) {
   return true;
 }
 
-inline int64_t get_merged_total_volume(const OrderMap &orders) {
-  std::unordered_map<uint32_t, int64_t> long_hash_to_total_merge_volume = {};
-  std::unordered_map<uint32_t, int64_t> short_hash_to_total_merge_volume = {};
-  for (auto &iter : orders) {
-    auto &order = iter.second;
-    auto instrument_key = hash_instrument(order.exchange_id, order.instrument_id);
-    auto direction = get_direction(order.instrument_type, order.side, order.offset);
-    auto is_long = direction == longfist::enums::Direction::Long;
-    auto &hash_to_total_merge_volume = is_long ? long_hash_to_total_merge_volume : short_hash_to_total_merge_volume;
-    int64_t final_merge_volume = (std::max)(hash_to_total_merge_volume[instrument_key], order.volume);
-    hash_to_total_merge_volume.insert_or_assign(instrument_key, final_merge_volume);
-  }
+// inline int64_t get_total_successfully_ordered_volume(const OrderMap &orders) {
+//   std::unordered_map<uint32_t, int64_t> long_hash_to_total_merge_volume = {};
+//   std::unordered_map<uint32_t, int64_t> short_hash_to_total_merge_volume = {};
+//   for (auto &iter : orders) {
+//     auto &order = iter.second;
+//     auto instrument_key = hash_instrument(order.exchange_id, order.instrument_id);
+//     auto direction = get_direction(order.instrument_type, order.side, order.offset);
+//     auto is_long = direction == longfist::enums::Direction::Long;
+//     auto &hash_to_total_merge_volume = is_long ? long_hash_to_total_merge_volume : short_hash_to_total_merge_volume;
+//     int64_t final_merge_volume = (std::max)(hash_to_total_merge_volume[instrument_key], order.volume);
+//     hash_to_total_merge_volume.insert_or_assign(instrument_key, final_merge_volume);
+//   }
 
-  int64_t total_volume = 0;
-  for (auto &iter : long_hash_to_total_merge_volume) {
-    total_volume += iter.second;
-  }
-  for (auto &iter : short_hash_to_total_merge_volume) {
-    total_volume += iter.second;
-  }
-  return total_volume;
-}
+//   int64_t total_volume = 0;
+//   for (auto &iter : long_hash_to_total_merge_volume) {
+//     total_volume += iter.second;
+//   }
+//   for (auto &iter : short_hash_to_total_merge_volume) {
+//     total_volume += iter.second;
+//   }
+//   return total_volume;
+// }
 
 struct BasketOrderState {
   kungfu::state<longfist::types::BasketOrder> state_data;
   OrderMap orders = {};
+  std::unordered_map<int32_t, int64_t> ordered_volume_map = {};
+  std::unordered_map<int32_t, int64_t> success_ordered_volume_map = {};
 
   BasketOrderState(uint32_t source, uint32_t dest, int64_t trigger_time,
                    const longfist::types::BasketOrder &basket_order);
 
   void update(const longfist::types::Order &order);
+  void update_success_ordered_volume(const longfist::types::Order &order, int32_t instrument_key);
 
   kungfu::state<longfist::types::BasketOrder> &get_state();
 
--- framework/core/src/libkungfu/wingchun/basketorder/basketorder.cpp
+++ framework/core/src/libkungfu/wingchun/basketorder/basketorder.cpp
@@ -13,14 +13,64 @@ BasketOrderState::BasketOrderState(uint32_t source, uint32_t dest, int64_t trigg
                                    const longfist::types::BasketOrder &basket_order)
     : state_data(state<BasketOrder>(source, dest, trigger_time, basket_order)){};
 
-void BasketOrderState::update(const longfist::types::Order &order) {
+void BasketOrderState::update_success_ordered_volume(const longfist::types::Order &order, int32_t instrument_key) {
 
-  orders.insert_or_assign(order.order_id, order);
+  bool is_new_order = (orders.find(order.order_id) == orders.end());
+  int64_t success_ordered_volume;
+  auto it = success_ordered_volume_map.find(instrument_key);
+  if (it != success_ordered_volume_map.end()) {
+    success_ordered_volume = it->second;
+  } else {
+    success_ordered_volume = 0;
+  }
+  if (is_new_order) {
+    if (order.status == OrderStatus::Error) {
+      // do nothing
+    } else if (order.status == OrderStatus::PartialFilledNotActive || order.status == OrderStatus::Cancelled) {
+      success_ordered_volume += order.volume - order.volume_left;
+    } else {
+      success_ordered_volume += order.volume;
+    }
+  } else {
+    if (order.status == OrderStatus::Error || order.status == OrderStatus::PartialFilledNotActive ||
+        order.status == OrderStatus::Cancelled) {
+      success_ordered_volume -= order.volume_left;
+    }
+  }
+  success_ordered_volume_map.insert_or_assign(instrument_key, success_ordered_volume);
+}
+
+void BasketOrderState::update(const longfist::types::Order &order) {
+  int32_t instrument_key = hash_instrument(order.exchange_id, order.instrument_id);
+  auto direction = get_direction(order.instrument_type, order.side, order.offset);
+  if (direction == Direction::Short) {
+    instrument_key = instrument_key * (-1);
+  }
   auto &basket_order = state_data.data;
+  int64_t total_volume = 0;
+  // if it is a new order
+  if (orders.find(order.order_id) == orders.end()) {
+    int64_t ordered_volume = 0;
+    auto it = success_ordered_volume_map.find(instrument_key);
+    if (it != success_ordered_volume_map.end()) {
+      ordered_volume = it->second;
+    } else {
+      ordered_volume = 0;
+    }
+    ordered_volume += order.volume;
+    ordered_volume_map.insert_or_assign(instrument_key, ordered_volume);
+    for (auto &iter : ordered_volume_map) {
+      total_volume += iter.second;
+    }
+  } else {
+    total_volume = basket_order.volume;
+  }
+  update_success_ordered_volume(order, instrument_key);
+  orders.insert_or_assign(order.order_id, order);
 
   // after supplementing order, the total volume may be changed, bigger than the original volume
   // for algo trade, the volume is hard to be cacluated, required to be set at init;
-  basket_order.volume = std::max(basket_order.volume, get_merged_total_volume(orders));
+  basket_order.volume = total_volume;
   basket_order.volume_left = basket_order.volume - get_total_traded_volume(orders);
   auto is_all_order_end_val = is_all_order_end(orders);
   auto is_all_order_filled_val = basket_order.volume_left == 0;
