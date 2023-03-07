bedc368c8fcb4348567c519a8c7c533b3bd371d0
yizhi <zhangyiizhi@126.com>
2023-03-07 17:24:07 +0800

    [fix] pm2

--- framework/api/src/utils/pm2Custom.ts
+++ framework/api/src/utils/pm2Custom.ts
@@ -2,7 +2,7 @@ import pm2 from 'pm2';
 import os from 'os';
 import path from 'path'
 
-process.env.OVER_HOME = path.resolve(os.homedir());
+process.env.OVER_HOME = path.resolve(path.join(os.homedir(), '.pm2'));
 if (process.env.NODE_ENV === 'development') {
   process.env.PM2_DEBUG = true;
 }
