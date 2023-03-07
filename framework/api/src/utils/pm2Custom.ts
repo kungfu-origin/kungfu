7aa1228cafe4f3aa856699676b3c4d6e06f1f680
yizhi <zhangyiizhi@126.com>
2023-03-07 14:49:00 +0800

    [format]

--- framework/api/src/utils/pm2Custom.ts
+++ framework/api/src/utils/pm2Custom.ts
@@ -1,6 +1,6 @@
 import pm2 from 'pm2';
 import os from 'os';
 
-const pm2Custom = new pm2['custom']({pm2_home: os.homedir()});
+const pm2Custom = new pm2['custom']({ pm2_home: os.homedir() });
 
 export default pm2Custom;
