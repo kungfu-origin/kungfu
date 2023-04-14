72fa761ec4b19ccc487c4d393644d1368c144f1b
yizhi <zhangyiizhi@126.com>
2023-04-14 15:49:52 +0800

    [format]

--- framework/core/src/bindings/node/binding/journal.cpp
+++ framework/core/src/bindings/node/binding/journal.cpp
@@ -204,7 +204,7 @@ std::vector<locator_ptr> Assemble::ExtractLocator(const Napi::CallbackInfo &info
   std::vector<locator_ptr> result = {};
   // auto locators = info[0].As<Napi::Array>();
   // for (int i = 0; i < locators.Length(); i++) {
-    // result.push_back(IODevice::GetLocator(locators, i));
+  // result.push_back(IODevice::GetLocator(locators, i));
   // }
   return result;
 }
