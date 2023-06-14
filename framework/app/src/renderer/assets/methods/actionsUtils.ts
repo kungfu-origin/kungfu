8ec00d1e22667a2588accbd1fe7099637d733b6f
yizhi <zhangyiizhi@126.com>
2023-06-14 10:33:46 +0800

    [fix] update ui sub logic

--- framework/app/src/renderer/assets/methods/actionsUtils.ts
+++ framework/app/src/renderer/assets/methods/actionsUtils.ts
@@ -1108,7 +1108,7 @@ export const useSubscibeInstrumentAtEntry = (
 
     return positions
       .reverse()
-      .slice(0, 500)
+      .slice(0, 256)
       .map((item: KungfuApi.Position): KungfuApi.InstrumentForSub => {
         const uidKey = hashInstrumentUKey(item.instrument_id, item.exchange_id);
         return {
