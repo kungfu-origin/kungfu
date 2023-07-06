7d26dd8646112812ec4ab38e1daca05e30555634
lm <1813933328@qq.com>
2023-07-06 10:15:01 +0800

    [fix] Fix frozen current warehouse calculation logic

--- framework/app/src/components/modules/makeOrder/MakeOrder.vue
+++ framework/app/src/components/modules/makeOrder/MakeOrder.vue
@@ -675,15 +675,9 @@ const handlePercentChange = (target: number) => {
     const availMoney = dealStringToNumber(currentAvailMoney.value);
     const allVolume = currentPrice.value ? availMoney / currentPrice.value : 0;
     targetVolume = allVolume * targetPercent;
-  } else if (curOffset === OffsetEnum.Close) {
+  } else {
     const availPosVolume = dealStringToNumber(currentAvailPosVolume.value);
     targetVolume = availPosVolume * targetPercent;
-  } else if (curOffset === OffsetEnum.CloseToday) {
-    const availPosVolume = currentPosition.value?.volume;
-    targetVolume = Number(availPosVolume) * targetPercent;
-  } else if (curOffset === OffsetEnum.CloseYest) {
-    const availPosVolume = currentPosition.value?.yesterday_volume;
-    targetVolume = Number(availPosVolume) * targetPercent;
   }
 
   formState.value.volume = dealVolumeByInstrumentType(
--- framework/app/src/renderer/assets/methods/actionsUtils.ts
+++ framework/app/src/renderer/assets/methods/actionsUtils.ts
@@ -2154,9 +2154,10 @@ export const useMakeOrderInfo = (
     const { offset } = formState.value;
 
     if (currentPosition.value) {
-      const { yesterday_volume, volume, frozen_total } = currentPosition.value;
+      const { yesterday_volume, volume, frozen_total, frozen_yesterday } =
+        currentPosition.value;
       const today_volume = volume - yesterday_volume;
-      const frozen_today = frozen_total - frozen_total;
+      const frozen_today = frozen_total - frozen_yesterday;
       const closable_yesterday = yesterday_volume - frozen_total;
       const closable_today = today_volume - frozen_today;
       const closable_total = volume - frozen_total;
